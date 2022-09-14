034f1e6c16689c9a409048fde29ccdcdb56afd58
Mathias Ho <you@example.com>
2022-09-14 17:49:18 +0800

    exp get by id and clone

--- prototype.py
+++ prototype.py
@@ -1,13 +1,35 @@
 from clearml.datasets import Dataset
 from fastapi import FastAPI
-
+from clearml.backend_api.session.client import APIClient
 app = FastAPI()
+client = APIClient()
+import inspect
+from pydantic import BaseModel
+from typing import Union
 
+class ClonePackage(BaseModel):
+    id: str
+    clone_name: Union[str, None] = None
 
 @app.get("/")
 async def hello_world():
     return {"message": "hello world"}
 
+
+@app.get("/experiments/{id}")
+async def get_experiment(id: str):
+    exp_list = client.tasks.get_by_id(task=id)
+    return {"id": exp_list.data.id, "name" : exp_list.data.name}
+    
+@app.post("/experiments/clone")
+async def clone_exp(item: ClonePackage):
+    exp_list = client.tasks.get_by_id(task=item.id)
+    if item.clone_name == None:
+        new_exp = exp_list.clone(new_task_name = f'Clone of {exp_list.data.name}')
+    else: 
+        new_exp = exp_list.clone(new_task_name = f'{item.clone_name}')
+    cloned = client.tasks.get_by_id(task=new_exp.id)
+    return {"id": exp_list.data.id, "name" : exp_list.data.name,"clone_id": cloned.data.id,"clone_name":cloned.data.name}
 
 @app.get("/datasets")
 async def get_all_datasets():
