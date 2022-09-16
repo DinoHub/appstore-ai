cdde7ff10c6e44aa0f0334b0bcdbcfb57492df87
Tien-Cheng <tiencheng@protonmail.com>
2022-09-16 11:54:03 +0800

    [feat] Use Pydantic settings for configuration

--- internal/db.py
+++ internal/db.py
@@ -1,9 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from config.config import config
 
-# TODO: Get MONGODB_URL from config file
-MONGODB_URL = "localhost:27017"
-
-mongo_client = AsyncIOMotorClient(MONGODB_URL)
+mongo_client = AsyncIOMotorClient(config.MONGODB_URL)
 
 db = mongo_client.app_store
 
