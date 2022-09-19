827fcc9dd67bb1e66bdc28afa4da41a889737e10
Mathias Ho <you@example.com>
2022-09-19 09:16:48 +0800

    auth key

--- routers/users.py
+++ routers/users.py
@@ -32,6 +32,36 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     return pwd_context.hash(password)
 
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = await db["users"].find_one({"username": token_data.username})
+    if user is None:
+        raise credentials_exception
+    return user 
+
+
 @router.post('/add')
 async def add_user(item: UserInsert):
     try:
@@ -65,4 +95,26 @@ async def update_user(user:  List [UserInsert]):
             except:
                 raise HTTPException(status_code=404, detail=f"Not found")
         return Response(status_code=204)
-    
+
+
+
+@router.post("/sys/auth",response_model=Token)
+async def auth_admin(form_data: OAuth2PasswordRequestForm = Depends()):
+    if (user := await db["users"].find_one({"username": form_data.username})) is not None:
+        if verify_pw(form_data.password,user['password']) is True:
+            if user['admin_priv'] is True:
+                access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+                access_token = create_access_token(data={"sub": user['username']}, expires_delta=access_token_expires)
+                return {"access_token": access_token, "token_type": "bearer"}
+            else:
+                raise HTTPException(status_code=401,detail=f"User is not admin",headers={"WWW-Authenticate": "Bearer"})
+        else:
+            raise HTTPException(status_code=401,detail=f"Password is wrong",headers={"WWW-Authenticate": "Bearer"})
+    else:
+        raise HTTPException(status_code=404,detail=f"Username does not exist",headers={"WWW-Authenticate": "Bearer"})
+
+@router.get("/sys/user", response_model=User)
+async def check_sys_user(current_user: User = Depends(get_current_user)):
+    if current_user['admin_priv'] is True:
+        return current_user
+    raise HTTPException(status_code=401,detail=f"User is not admin",headers={"WWW-Authenticate": "Bearer"})
