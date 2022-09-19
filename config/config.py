d423a8d0b2782ee60f0fb28ae87f8a2cba348549
Tien-Cheng <tiencheng@protonmail.com>
2022-09-19 13:58:51 +0800

    [config] Add MAX_UPLOAD_SIZE_GB envvar

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 # https://rednafi.github.io/digressions/python/2020/06/03/python-configs.html
-from typing import Optional
+from typing import Optional, Union
 
 from pydantic import BaseModel, BaseSettings, Field
 
@@ -8,14 +8,18 @@ from pydantic import BaseModel, BaseSettings, Field
 
 class GlobalConfig(BaseSettings):
     ENV_STATE: Optional[str] = Field(None, env="ENV_STATE")
-    MONGODB_URL : Optional[str] = None
+    MONGODB_URL: Optional[str] = None
+    MAX_UPLOAD_SIZE_GB: Optional[Union[int, float]] = None
+
     class Config:
-        env_file: str = ".env"
+        env_file: str = "config/.env"
+
 
 class DevConfig(GlobalConfig):
     class Config:
         env_prefix: str = "DEV_"
         # TODO: add secrets_dir to support loading secrets
+
 
 class StagingConfig(GlobalConfig):
     class Config:
@@ -28,11 +32,13 @@ class ProductionConfig(GlobalConfig):
         env_prefix: str = "PROD_"
         # TODO: add secrets_dir to support loading secrets
 
+
 class FactoryConfig:
     """Return config instance based on `ENV_STATE` variable"""
+
     def __init__(self, env_state: Optional[str]):
         self.env_state = env_state
-    
+
     def __call__(self):
         if self.env_state == "dev":
             return DevConfig()
@@ -40,6 +46,7 @@ class FactoryConfig:
             return StagingConfig()
         elif self.env_state == "prod":
             return ProductionConfig()
+
 
 config = FactoryConfig(GlobalConfig().ENV_STATE)()
 print(config.__repr__())
