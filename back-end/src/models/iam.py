1940c52c21b023012222e0f4b33927da4992d44d
Mathias Ho <osirishyp@gmail.com>
2022-09-24 23:01:52 +0800

    update iam routes/models w confirm pw + pagination

--- back-end/src/models/iam.py
+++ back-end/src/models/iam.py
@@ -1,26 +1,41 @@
 from typing import Dict, List, Optional, Union
-
 from bson import ObjectId
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field ,ValidationError , validator
+from password_strength import PasswordPolicy
 
 from .common import PyObjectId
 
+policy = PasswordPolicy.from_names(
+    length=8,  # min length: 8
+    uppercase=1,  # need min. 2 uppercase letters
+    numbers=1,  # need min. 2 digits
+    special=1,  # need min. 2 special characters
+    )
 
-class UserEdit(BaseModel):
-    name: str
-    password: str
-    admin_priv: bool = False
 
 
 class UserInsert(BaseModel):
     userid: str
     name: str
     password: str
+    password_confirm: str
     admin_priv: bool = False
 
+    @validator('password_confirm')
+    def match_passwords(cls, v, values, **kwargs):
+        if 'password_confirm' in values and v != values['password']:
+            raise ValueError('Passwords do not match')
+        strength = policy.test(values['password'])
+        if not strength:
+            return v
+        raise ValueError('Password must at least be length of 8, have 1 uppercase letter, 1 number and 1 special character')
 
-class UserInsertDB(UserInsert):
+
+class UserInsertDB(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    userid: str
+    name: str
+    admin_priv: bool = False
 
     class Config:
         allow_population_by_field_name = True
@@ -43,3 +58,30 @@ class User(BaseModel):
 
 class UserInDB(User):
     hashed_password: str
+
+
+class UserPage(BaseModel):
+    user_num: int
+    name: str = ''
+    admin_priv: int = 0
+
+    @validator('user_num')
+    def num_of_user_more_than_one(cls, v):
+        if v <= 0:
+            raise ValueError('Number of users displayed must be more than one')
+        return v
+
+    @validator('name')
+    def name_is_empty(cls, v):
+        if v.strip() == '':
+            return None
+        return v
+    
+    @validator('admin_priv')
+    def admin_priv_check(cls, v):
+        if v is 1:
+            return True 
+        elif v is 2:
+            return False
+        else:
+            return None
--- back-end/src/routers/iam.py
+++ back-end/src/routers/iam.py
@@ -1,3 +1,4 @@
+from aifc import Error
 import json
 from typing import List, Mapping, Optional, Union
 from datetime import datetime, timedelta
@@ -15,7 +16,7 @@ from passlib.context import CryptContext
 from ..config.config import admin
 from ..internal.clearml_client import clearml_client
 from ..internal.db import db, mongo_client
-from ..models.iam import UserInsert, UserInsertDB, UserEdit, Token, TokenData, User 
+from ..models.iam import UserInsert, UserInsertDB, Token, TokenData, User ,UserPage
 from pymongo import errors as pyerrs
 
 
@@ -71,17 +72,17 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 async def add_user(item: UserInsert, current_user: User = Depends(get_current_user)):
     try:
         item.password = get_password_hash(item.password)
-        item = jsonable_encoder(item)
-        user = await db["users"].insert_one(item)
-        add_user = await db["users"].find_one({"_id": user.inserted_id})
-        add_user = jsonable_encoder(UserInsertDB(**add_user))
-        return JSONResponse(status_code=status.HTTP_201_CREATED, content=add_user)
+        user = await db["users"].insert_one({"userid": item.userid,"name": item.name,"password": item.password,"admin_priv":item.admin_priv})
+        add_user = await db["users"].find_one({"_id": user.inserted_id},{"_id":False,"password":False})
+        print(add_user)
+        return JSONResponse(status_code=status.HTTP_201_CREATED, content=f"User of ID: {add_user['userid']} created")
     except pyerrs.DuplicateKeyError:
         return JSONResponse(
             status_code=status.HTTP_409_CONFLICT,
             content=f"Userid of {item.userid} already exists",
         )
-    except:
+    except Error as e:
+        print(e)
         return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content="An error occurred",
@@ -100,18 +101,18 @@ async def delete_user(userid: List[str], current_user: User = Depends(get_curren
 
 
 @router.put("/edit")
-async def update_user(user: List[UserInsert], current_user: User = Depends(get_current_user)):
-    id_list = []
-    for x in user:
-        try:
-            x.password = get_password_hash(x.password)
-            update_result = await db["users"].update_one(
-                {"userid": x.userid},
-                {"$set": jsonable_encoder(UserEdit(**jsonable_encoder(x)))},
+async def update_user(user: UserInsert, current_user: User = Depends(get_current_user)):
+    try:
+        user.password = get_password_hash(user.password)
+        update_result = await db["users"].update_one(
+                {"userid": user.userid},
+                {"$set": {"userid": user.userid,"name": user.name,"password": user.password,"admin_priv":user.admin_priv}},
             )
-            id_list.append(x.userid)
-        except:
-            raise HTTPException(status_code=404, detail=f"Not found")
+    except ValueError as e:
+        raise HTTPException(status_code= 422, detail=f'')
+
+    except:
+        raise HTTPException(status_code=404, detail=f"Not found")
     return Response(status_code=204)
 
 
@@ -144,7 +145,7 @@ async def auth_user_admin(form_data: OAuth2PasswordRequestForm = Depends()):
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-
+# check whether user is authenticated and has admin priv
 @router.get("/check", response_model=User)
 async def check_user_admin(current_user: User = Depends(get_current_user)):
     if current_user["admin_priv"] is True:
@@ -155,24 +156,34 @@ async def check_user_admin(current_user: User = Depends(get_current_user)):
         headers={"WWW-Authenticate": "Bearer"},
     )
 
-@router.get("/{page_size}/{page_num}")
-async def get_users(page_size: int  = Path(ge = 1), page_num: int = Path(ge = 1),current_user: User = Depends(get_current_user)):
+# list users with pagination
+@router.post("/{page_num}")
+async def get_users(pages_user : UserPage, page_num: int = Path(ge = 1),current_user: User = Depends(get_current_user), ):
         if current_user["admin_priv"] is True:
-            # check number of documents to skip past
-            skips = page_size * (page_num - 1)
-
-            # dont skip if 1st page
-            if skips <= 0: 
-                # find from users in MongodDB exclude ObjectID
-                cursor =  db['users'].find({}, {'_id': False}).limit(page_size)
-            # else call cursor with skips
-            else:
-                # find from users in MongodDB exclude ObjectID
-                cursor =  db['users'].find({}, {'_id': False}).skip(skips).limit(page_size)
-            # get from MongoDB and convert to list
-            cursor =  await cursor.to_list(length = page_size)
-            # return documents
-            return JSONResponse(status_code=status.HTTP_200_OK, content=cursor)
-        else:
-            # ensure when loading this endpoint that user is authenticated
-            raise HTTPException(status_code = 401,detail=f"User is not admin",headers={"WWW-Authenticate": "Bearer"})
+            try:
+                # check number of documents to skip past
+                skips = pages_user.user_num * (page_num - 1)
+                # lookups for username and admin priv matching
+                lookup = {}
+                if pages_user.name != None:
+                    lookup['username'] = '/.*'+pages_user.name+'.*/i'
+                if pages_user.admin_priv != None:
+                    lookup['admin_priv'] = pages_user.admin_priv 
+                # dont skip if 1st page
+                if skips <= 0: 
+                    # find from users in MongodDB exclude ObjectID
+                    cursor =  db['users'].find({}, {'_id': False, 'password' : False}).limit(pages_user.user_num)
+                # else call cursor with skips
+                else:
+                    # find from users in MongodDB exclude ObjectID
+                    cursor =  db['users'].find(lookup, {'_id': False, 'password' : False}).skip(skips).limit(pages_user.user_num)
+                # get from MongoDB and convert to list
+                cursor =  await cursor.to_list(length = pages_user.user_num)
+                # return documents
+                return JSONResponse(status_code=status.HTTP_200_OK, content=cursor)
+            except ValueError as e:
+                return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, content=e)
+            except:
+                return HTTPException(status_code=404)
+        # ensure when loading this endpoint that user is authenticateds not admin",headers={"WWW-Authenticate": "Bearer"})
+        raise HTTPException(status_code = 401,detail=f"User is authenticated",headers={"WWW-Authenticate": "Bearer"})
