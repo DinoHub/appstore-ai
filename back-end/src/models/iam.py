d46a050c45d18100d703bb83de2a65e791169a36
Mathias Ho <osirishyp@gmail.com>
2022-09-26 11:53:05 +0800

    update iam pw policy

--- back-end/src/models/iam.py
+++ back-end/src/models/iam.py
@@ -7,9 +7,9 @@ from .common import PyObjectId
 
 policy = PasswordPolicy.from_names(
     length=8,  # min length: 8
-    uppercase=1,  # need min. 2 uppercase letters
-    numbers=1,  # need min. 2 digits
-    special=1,  # need min. 2 special characters
+    uppercase=1,  # need min. 1 uppercase letters
+    numbers=1,  # need min. 1 digits
+    special=1,  # need min. 1 special characters
     )
 
 
