8ccad39c35a5f90d4ab9f4fec27b21a6c4b9e1ea
Tien-Cheng <tiencheng@protonmail.com>
2022-09-27 16:47:06 +0800

    [fix] Allow clearml to load local credentials if envvar not provided

--- back-end/src/internal/clearml_client.py
+++ back-end/src/internal/clearml_client.py
@@ -1,11 +1,18 @@
-from clearml.backend_api.session.client import APIClient
-from ..config.config import clear_conf
 import os
 
-os.environ['CLEARML_WEB_HOST'] = clear_conf.CLEARML_WEB_HOST
-os.environ['CLEARML_API_HOST'] = clear_conf.CLEARML_API_HOST
-os.environ['CLEARML_FILES_HOST'] = clear_conf.CLEARML_FILES_HOST
-os.environ['CLEARML_API_ACCESS_KEY'] = clear_conf.CLEARML_API_ACCESS_KEY
-os.environ['CLEARML_API_SECRET_KEY'] = clear_conf.CLEARML_API_SECRET_KEY
+from clearml.backend_api.session.client import APIClient
+
+from ..config.config import clear_conf
+
+try:
+    os.environ["CLEARML_WEB_HOST"] = clear_conf.CLEARML_WEB_HOST
+    os.environ["CLEARML_API_HOST"] = clear_conf.CLEARML_API_HOST
+    os.environ["CLEARML_FILES_HOST"] = clear_conf.CLEARML_FILES_HOST
+    os.environ["CLEARML_API_ACCESS_KEY"] = clear_conf.CLEARML_API_ACCESS_KEY
+    os.environ["CLEARML_API_SECRET_KEY"] = clear_conf.CLEARML_API_SECRET_KEY
+except TypeError as e:
+    print(
+        "Warning: Failed to set ClearML envvars. It will rely on your local configuration"
+    )
 
 clearml_client = APIClient()
