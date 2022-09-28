db1e17dd5ec5c6c4eed7826547d5f8d4609a3197
Tien-Cheng <tiencheng@protonmail.com>
2022-09-28 17:16:33 +0800

    [wip] init files for tests

--- ai-model/inference-engine/inference_engine/schemas/io/__init__.py
+++ ai-model/inference-engine/inference_engine/schemas/io/__init__.py
@@ -1,1 +1,7 @@
-from IOSchema import IOSchema
+from .GenericIO import GenericIO
+from .IOSchema import IOSchema
+from .MultipleMediaFileIO import MultipleMediaFileIO
+from .SingleMediaFileIO import SingleMediaFileIO
+from .TextIO import TextIO
+
+__all__ = ["GenericIO", "MultipleMediaFileIO", "SingleMediaFileIO", "TextIO"]
