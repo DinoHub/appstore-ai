d2ef310d2b60fef6028607dec91b6550796cd1ba
Tien-Cheng <tiencheng@protonmail.com>
2022-09-29 17:31:12 +0800

    [feat] Add utility class to make defining mime types easier

--- ai-model/inference-engine/inference_engine/__init__.py
+++ ai-model/inference-engine/inference_engine/__init__.py
@@ -1,2 +1,3 @@
 from .core.engine import InferenceEngine
 from .schemas.io import *
+from .schemas.media_types import media_type
---
+++ ai-model/inference-engine/inference_engine/schemas/media_types.py
@@ -0,0 +1,9 @@
+class media_type:
+    plain_text = "text/plain"
+    jpeg = "image/jepg"
+    png = "image/png"
+    webp = "image/webp"
+    wav = "audio/wav"
+    mpeg = "audio/mpeg"
+    ogg = "audio/ogg"
+    webm = "video/webm"
