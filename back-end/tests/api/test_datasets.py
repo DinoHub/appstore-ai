869cd7d5e7e89443b9b359b374b59a3f775d9e1d
Tien-Cheng <tiencheng@protonmail.com>
2022-10-04 09:56:19 +0800

    [fix] Fix Dataset delete method not working

--- back-end/tests/api/test_datasets.py
+++ back-end/tests/api/test_datasets.py
@@ -4,6 +4,8 @@ import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 
+from src.internal.data_connector import ClearMLDataset
+
 
 def test_get_all_datasets(
     client: TestClient,
@@ -12,16 +14,14 @@ def test_get_all_datasets(
     response_json = response.json()
     assert response.status_code == status.HTTP_200_OK
     assert isinstance(response_json, list)
-    # since is demo server, possible that someone added extra
-    # but at minimum, clearml has 2 example datasets
+    # might be more than 2 if fail to clean up test
     assert len(response_json) >= 2
 
 
 @pytest.mark.parametrize(
     "project",
     [
-        "ClearML - Nvidia Framework Examples/TLTv3",
-        "ClearML - Nvidia Framework Examples/Clara",
+        "ClearML examples/Urbansounds",
     ],
 )
 def test_get_datasets_by_project(
@@ -32,13 +32,13 @@ def test_get_datasets_by_project(
     response = client.post("/datasets/search", json={"project": project})
     response_json = response.json()
     assert response.status_code == status.HTTP_200_OK
-    assert len(response_json) == 1  # both projects should have only 1 dataset
+    assert len(response_json) == 2
     assert isinstance(response_json, list)
     for dataset in response_json:
         assert dataset["project"] == project
 
 
-@pytest.mark.parametrize("dataset_id", ["9751c847f6664f52a096e1264b258fad"])
+@pytest.mark.parametrize("dataset_id", ["e-f581e44aa3ee42f68206f3ec5d4b1ebc"])
 def test_get_dataset_by_id(dataset_id: str, client: TestClient):
     response = client.get(f"/datasets/{dataset_id}")
     assert response.status_code == status.HTTP_200_OK
@@ -59,5 +59,9 @@ def test_create_dataset(file_path: str, client: TestClient):
         },
         files={"file": open(Path(__file__).parent.joinpath(file_path), "rb")},
     )
-    print(response.json())
+    dataset = response.json()
     assert response.status_code == status.HTTP_201_CREATED
+    # assert dataset["name"] == "dataset_42"
+    # assert dataset["project"] == "test_create_dataset"
+    # Perform cleanup of data
+    ClearMLDataset.get(id=dataset["id"]).delete()
