7c6adfae8949be3cb767bd275e6f85a02d7ae6c3
Tien-Cheng <tiencheng@protonmail.com>
2022-10-04 09:11:47 +0800

    [fix] Fix stable diffusion IE registering wrong type

--- ai-model/examples/stable_diffusion/main.py
+++ ai-model/examples/stable_diffusion/main.py
@@ -1,15 +1,8 @@
 from executor import predict
-from inference_engine import (
-    InferenceEngine,
-    SingleMediaFileIO,
-    TextIO,
-    media_type,
-)
+from inference_engine import InferenceEngine, MediaFileIO, TextIO, media_type
 
 engine = InferenceEngine.from_yaml("meta.yaml")
-engine.entrypoint(
-    predict, TextIO, SingleMediaFileIO, media_type=media_type.jpeg
-)
+engine.entrypoint(predict, TextIO, MediaFileIO, media_type=media_type.jpeg)
 
 if __name__ == "__main__":
     engine.serve()
