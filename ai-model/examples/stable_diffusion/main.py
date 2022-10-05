d1f1d28e36f841ab630a6688b4b9a9121012f5a0
Tien-Cheng <tiencheng@protonmail.com>
2022-10-05 16:16:29 +0800

    [refactor] Update examples to new format

--- ai-model/examples/stable_diffusion/main.py
+++ ai-model/examples/stable_diffusion/main.py
@@ -1,7 +1,7 @@
-from executor import predict
 from inference_engine import InferenceEngine, MediaFileIO, TextIO, media_type
+from process import predict
 
-engine = InferenceEngine.from_yaml("meta.yaml")
+engine = InferenceEngine.from_yaml("config.yaml")
 engine.entrypoint(predict, TextIO, MediaFileIO, media_type=media_type.jpeg)
 
 if __name__ == "__main__":
