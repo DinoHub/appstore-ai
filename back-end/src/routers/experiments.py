af91798c6d550c615075982f3e4e190a5312f19c
Tien-Cheng <tiencheng@protonmail.com>
2022-10-06 17:48:48 +0800

    [refactor] Sort imports with isort

--- back-end/src/routers/experiments.py
+++ back-end/src/routers/experiments.py
@@ -1,4 +1,5 @@
 from typing import Dict
+
 from fastapi import APIRouter
 
 from ..internal.clearml_client import clearml_client
@@ -10,14 +11,20 @@ router = APIRouter(prefix="/experiments", tags=["Experiments"])
 @router.get("/{id}")
 async def get_experiment(id: str):
     exp_list = clearml_client.tasks.get_by_id(task=id)
-    return {"id": exp_list.data.id, "name": exp_list.data.name, "rest": exp_list.data}
+    return {
+        "id": exp_list.data.id,
+        "name": exp_list.data.name,
+        "rest": exp_list.data,
+    }
 
 
 @router.post("/clone")
 async def clone_experiment(item: ClonePackageModel):
     exp_list = clearml_client.tasks.get_by_id(task=item.id)
     if item.clone_name == None:
-        new_exp = exp_list.clone(new_task_name=f"Clone of {exp_list.data.name}")
+        new_exp = exp_list.clone(
+            new_task_name=f"Clone of {exp_list.data.name}"
+        )
     else:
         new_exp = exp_list.clone(new_task_name=f"{item.clone_name}")
     cloned = clearml_client.tasks.get_by_id(task=new_exp.id)
