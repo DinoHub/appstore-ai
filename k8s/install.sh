3418ed86efa5d1eebbc63a39a7fa5b826623d58b
Tien-Cheng <tiencheng@protonmail.com>
2022-10-06 11:32:48 +0800

    [feat] Helm chart to set up KNative

--- k8s/install.sh
+++ k8s/install.sh
@@ -11,12 +11,13 @@ kubectl apply -f metallb-config.yaml -n metallb-system
 
 # Set up KNative
 kubectl apply -f https://github.com/knative/operator/releases/download/knative-v1.7.1/operator.yaml
-kubectl apply -f knative/knative-serving.yaml
+kubectl create namespace knative-serving
+# kubectl apply -f knative/knative-serving.yaml
 
 # Set up Magic DNS
-kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.7.2/serving-default-domain.yaml
+# kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.7.2/serving-default-domain.yaml
 # Make KNative Services Private (only allow within cluster call)
 # kubectl apply -f knative/domain.yaml
 
 # Create namespace for engines
-kubectl create namespace ie
+# kubectl create namespace ie
