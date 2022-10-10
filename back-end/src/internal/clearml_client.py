21f21b39358e329c719a72ecf238b90acf26c2d7
Tien-Cheng <tiencheng@protonmail.com>
2022-10-10 15:07:35 +0800

    [refactor] Format files with black

--- back-end/src/internal/clearml_client.py
+++ back-end/src/internal/clearml_client.py
@@ -10,7 +10,7 @@ try:
     os.environ["CLEARML_FILES_HOST"] = clear_conf.CLEARML_FILES_HOST
     os.environ["CLEARML_API_ACCESS_KEY"] = clear_conf.CLEARML_API_ACCESS_KEY
     os.environ["CLEARML_API_SECRET_KEY"] = clear_conf.CLEARML_API_SECRET_KEY
-    print('ClearML ENV vars set')
+    print("ClearML ENV vars set")
 except TypeError as e:
     print(
         "Warning: Failed to set ClearML envvars. It will rely on your local configuration"
