f819d672348a9b79327465b69464545bdd99c345
Tien-Cheng <tiencheng@protonmail.com>
2022-10-11 09:39:03 +0800

    [refactor] Remove deprecated pydantic model

--- back-end/src/models/common.py
+++ back-end/src/models/common.py
@@ -17,7 +17,3 @@ class PyObjectId(ObjectId):
     @classmethod
     def __modify_schema__(cls, field_schema):
         field_schema.update(type="string")
-
-
-class SecurityClassificationModel(str, Enum):
-    unclassified = "unclassified"
