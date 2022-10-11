16279fc068430a84866faa0f4a599292c3508423
Tien-Cheng <tiencheng@protonmail.com>
2022-10-11 11:25:53 +0800

    [refactor] Put CLI inside __init__ of module instead of __main__

--- ai-model/inference-engine/inference_engine/__init__.py
+++ ai-model/inference-engine/inference_engine/__init__.py
@@ -1,2 +1,16 @@
+import click
+
+from .commands.new import new
 from .core.engine import InferenceEngine
 from .schemas.io import *
+
+
+# Create new inference engine
+@click.group()
+@click.version_option()
+def cli():
+    """Command line interface for Inference Engine."""
+    pass
+
+
+cli.add_command(new)
--- ai-model/inference-engine/inference_engine/__main__.py
+++ ai-model/inference-engine/inference_engine/__main__.py
@@ -1,18 +1,4 @@
-import click
-
-from .commands.new import new
-
-
-# Create new inference engine
-@click.group()
-@click.version_option()
-def cli():
-    """Command line interface for Inference Engine."""
-    pass
-
-
-cli.add_command(new)
-
+from . import cli
 
 if __name__ == "__main__":
     cli()
