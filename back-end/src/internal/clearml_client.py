fea7c79d48cd875235f2e2c2eb366be169b6baf3
Tien-Cheng <tiencheng@protonmail.com>
2022-10-13 13:09:17 +0800

    [fix] Fix not setting default value for dependency

--- back-end/src/internal/clearml_client.py
+++ back-end/src/internal/clearml_client.py
@@ -6,7 +6,9 @@ from clearml.backend_api.session.client import APIClient, StrictSession
 from ..config.config import config
 
 
-def clearml_api_client(config_path: Optional[Union[str, Path]]) -> APIClient:
+def clearml_api_client(
+    config_path: Optional[Union[str, Path]] = None
+) -> APIClient:
     if not config_path:
         config_path = config.CLEARML_CONFIG_FILE
     session = StrictSession(config_file=config_path)
