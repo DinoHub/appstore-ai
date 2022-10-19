e15faaf1615b606d3faac20ccf7fd542b6665606
Mathias Ho <osirishyp@gmail.com>
2022-10-19 14:53:44 +0800

    added new gradio app for zero shot class nlp

--- ai-model/examples/ZeroShotClass_XLMRob/app.py
+++ ai-model/examples/ZeroShotClass_XLMRob/app.py
@@ -1,29 +1,13 @@
-import argparse
+from json import load
+import gradio as gr
 import numpy as np
 import sys
-from functools import partial
-import os
-import tritonclient
 import tritonclient.grpc as tritongrpc
-from tritonclient.utils import triton_to_np_dtype
-from tritonclient.utils import InferenceServerException
 from transformers import XLMRobertaTokenizer
 from scipy.special import softmax
 from datetime import datetime
 
-# load tokeniser
-R_tokenizer = XLMRobertaTokenizer.from_pretrained('joeddav/xlm-roberta-large-xnli')
-VERBOSE = False
-
-# set up input for triton
-input_name = ['input__0', 'input__1']
-output_name = 'output__0'
-
-def run_inference(premise = '',topic = '', model_name='zst', url='127.0.0.1:8001', model_version='1'):
-    topic = topic
-    print(f'\n[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] PREMISE: {premise}')
-    print(f'[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] TOPIC: {topic}')
-
+def loadModel(model_name='zst', model_version='1', url='127.0.0.1:8001'):
     # establish connection to triton
     triton_client = tritongrpc.InferenceServerClient(
         url=url, verbose=VERBOSE)
@@ -33,16 +17,30 @@ def run_inference(premise = '',topic = '', model_name='zst', url='127.0.0.1:8001
     triton_client.load_model(model_name)
     if not triton_client.is_model_ready(model_name):
         print(f'[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] FAILED: Load Model')
-        sys.exit(1) 
     else:
         print(f'[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] PASSED: Load Model')
-        
-     # get model data
+
+    # get model data
     model_metadata = triton_client.get_model_metadata(
         model_name=model_name, model_version=model_version)
     model_config = triton_client.get_model_config(
         model_name=model_name, model_version=model_version)    
     print(f'[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] CONFIG: {model_config}')
+    return triton_client
+
+def unloadModel(triton_client, model_name='zst'):
+    # unload model from triton server
+    triton_client.unload_model(model_name)
+    if triton_client.is_model_ready(model_name):
+        print(f'\n[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] FAILED: Unload Model')
+    else:
+        print(f'\n[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] PASSED : Unload Model')
+
+def run_inference(triton_client, premise = '',topic = '',model_name='zst', model_version='1' , ):
+
+    topic = topic
+    print(f'\n[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] PREMISE: {premise}')
+    print(f'[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] TOPIC: {topic}')
 
     # tokenize input
     input_ids = R_tokenizer.encode(premise, topic, max_length=256, 
@@ -82,16 +80,43 @@ def run_inference(premise = '',topic = '', model_name='zst', url='127.0.0.1:8001
     print(f'[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] PREDICTION: Label has {true_prob:0.2f}% of being true')
     print(f'[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] PREDICTION: Label has {false_prob:0.2f}% of being false')
     
-    # unload model from triton server
-    triton_client.unload_model(model_name)
-    if triton_client.is_model_ready(model_name):
-        print(f'\n[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] FAILED: Unload Model')
-        sys.exit(1)
-    else:
-        print(f'\n[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] PASSED : Unload Model')
-    
+    return true_prob
 
+try:
+    # load tokeniser
+    R_tokenizer = XLMRobertaTokenizer.from_pretrained('joeddav/xlm-roberta-large-xnli')
+    VERBOSE = False
+    # def model name
+    model_name = "xlm_roberta_zsl"
+    model_version = "1"
+    # set up input for triton
+    input_name = ['input__0', 'input__1']
+    output_name = 'output__0'
 
-if __name__ == '__main__':
-    run_inference("He hath utterly destroyed them, he hath delivered them to the slaughter. Their slain also shall be cast out, and their stink shall come up out of their carcases, and the mountains shall be melted with their blood."
-    ,'This text is about killing and death.','xlm_roberta_zsl')
+    client = loadModel(model_name,model_version)
+
+    def question_answer(premise, topic):
+        splitTopic = list(filter(None, topic.split(",")))
+        list_output = []
+        styledOutput = ""
+        for x in splitTopic:
+            output = run_inference(client,premise,x.strip(),model_name,model_version)
+            list_output.append([x,output])
+        list_output = sorted(list_output,key = lambda x: x[1],reverse=True)
+        for i in list_output:
+            styledOutput += f"{i[0].strip()} ({round(i[1],2)}%)\n"
+        return styledOutput.strip()
+
+    with gr.Blocks() as demo:
+        premiseBox = gr.Textbox(placeholder="Text to classify...",label="Zero Shot Classification")
+        topicBox = gr.Textbox(placeholder="Possible class names...",label= "Possible class names (comma-separated)")
+
+        output = gr.Textbox(label="Probabilities",placeholder="<Label> (<Probability of being true>)")
+
+        comp_btn = gr.Button("Compute")
+        comp_btn.click(fn=question_answer,inputs = [premiseBox,topicBox] , outputs= output)
+
+    demo.launch()
+except:
+    unloadModel(client)
+    print(f'[{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}] Exited Application')
