0d100bf0edd8ea2a90d2b6f1262da3d5f511dfb0
Tien-Cheng <tiencheng@protonmail.com>
2022-10-25 13:00:27 +0800

    [refactor] Use composition API for pinia

--- front-end/src/stores/auth-store.ts
+++ front-end/src/stores/auth-store.ts
@@ -1,10 +1,16 @@
 import { defineStore } from 'pinia';
+import { Ref, ref } from 'vue';
 
-export const useAuthStore = defineStore({
-  id: 'auth',
-  state: () => ({
-    user: true, // tmp
-    returnUrl: null,
-  }),
-  actions: {},
-});
+// export const useAuthStore = defineStore({
+//   id: 'auth',
+//   state: () => ({
+//     user: true, // tmp
+//     returnUrl: null,
+//   }),
+//   actions: {},
+// });
+
+export const useAuthStore = defineStore('auth', () => {
+  const user = ref(true);
+  const returnUrl: Ref<null | string> = ref(null);
+})
