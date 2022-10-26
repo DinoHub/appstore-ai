33d88de2c1b5c5297924ae930f3c4029a7270613
Tien-Cheng <tiencheng@protonmail.com>
2022-10-26 13:20:17 +0800

    [refactor] Minor refactor of routing code

--- front-end/src/router/routes.ts
+++ front-end/src/router/routes.ts
@@ -3,13 +3,14 @@ import DashboardPage from 'pages/DashboardPage.vue';
 import ErrorNotFound from 'pages/ErrorNotFound.vue';
 import LoginPage from 'pages/LoginPage.vue';
 import MainLayout from 'layouts/MainLayout.vue';
+import ModelPage from 'pages/ModelPage.vue';
 import { RouteRecordRaw } from 'vue-router';
 import { useAuthStore } from 'src/stores/auth-store';
 const routes: RouteRecordRaw[] = [
   {
     path: '/login',
-    component: () => MainLayout,
-    children: [{ path: '', component: () => LoginPage }],
+    component: MainLayout,
+    children: [{ path: '', component: LoginPage }],
     beforeEnter: (to) => {
       const auth = useAuthStore();
       if (auth.access_token) {
@@ -20,25 +21,29 @@ const routes: RouteRecordRaw[] = [
   },
   {
     path: '/models',
-    component: () => MainLayout,
+    component: MainLayout,
     children: [
       {
+        path: ':id',
+        component: ModelPage,
+      },
+      {
         path: 'create',
-        component: () => CreateModel,
+        component: CreateModel,
       },
     ],
   },
   {
     path: '/',
-    component: () => MainLayout,
-    children: [{ path: '', component: () => DashboardPage }],
+    component: MainLayout,
+    children: [{ path: '', component: DashboardPage }],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => ErrorNotFound,
+    component: ErrorNotFound,
   },
 ];
 
