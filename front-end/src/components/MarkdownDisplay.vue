dd8546bed26d53be9adc5b74b80ce3a0d76b4fe7
Tien-Cheng <tiencheng@protonmail.com>
2022-10-26 14:58:02 +0800

    [feat] Add ability to render markdown safely

---
+++ front-end/src/components/MarkdownDisplay.vue
@@ -0,0 +1,18 @@
+<template>
+  <article v-html="convertedMarkdown"></article>
+</template>
+
+<script setup lang="ts">
+import MarkdownIt from 'markdown-it';
+import { defineProps, Ref, ref } from 'vue';
+interface Props {
+  markdown: string;
+}
+const props = defineProps<Props>();
+const md = new MarkdownIt();
+
+// Render Markdown
+const convertedMarkdown: Ref<string> = ref('');
+
+convertedMarkdown.value = md.render(props.markdown);
+</script>
