8762523744be3a186b802c0c551c9a95d1e12526
Tien-Cheng <tiencheng@protonmail.com>
2022-10-26 11:38:20 +0800

    [wip] Work on Dashboard

---
+++ front-end/src/components/ModelCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-card>
+    <q-card-section>
+      <h6>{{ title }}</h6>
+      <q-chip v-for="tag in tags" :key="tag" color="primary" text-color="white">
+        {{ tag }}
+      </q-chip>
+    </q-card-section>
+    <q-card-section>
+      {{ description }}
+    </q-card-section>
+    <q-separator></q-separator>
+    <q-card-actions>
+      <q-btn flat label="View" text-color="primary" :to="`models/${id}`"></q-btn>
+      <q-btn flat label="Edit" text-color="primary" :to="`models/${id}/edit`"></q-btn>
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue'
+
+export interface ModelCardProps {
+  id: string,
+  title: string
+  description: string
+  tags: string[],
+}
+const props = withDefaults(defineProps<ModelCardProps>(), {
+  title: 'Card Title',
+  description: 'Sample description',
+  tags: () => {
+    return ['Example 1', 'Example 2']
+  }
+})
+</script>
---
+++ front-end/src/pages/DashboardPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    <h1>Welcome Back, {{ username ?? 'User' }}</h1>
+    <main>
+      <section>
+        <div class="row">
+          <div class="col">
+            <h2>Your models</h2>
+          </div>
+          <div class="col">
+            <q-btn round icon="add"></q-btn>
+          </div>
+          <div class="col">
+            <q-btn to="/models" flat><span class="underline">See all models</span></q-btn>
+          </div>
+        </div>
+        <model-card></model-card>
+      </section>
+    </main>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import ModelCard from 'src/components/ModelCard.vue';
+import { useAuthStore } from 'src/stores/auth-store';
+import { useModelStore } from 'src/stores/model-store';
+
+const authStore = useAuthStore();
+const username = ref(authStore.user?.name);
+
+// Get all user models
+const modelStore = useModelStore()
+modelStore.getAll().then((data) => { console.log(data)} )
+// Get models owned by the user
+</script>
+
+<style>
+.underline {
+  text-decoration: underline;
+}
+</style>
