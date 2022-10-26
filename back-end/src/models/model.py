3e9780f5e51c317817ba45d0a32519534b1a532f
Tien-Cheng <tiencheng@protonmail.com>
2022-10-26 15:15:26 +0800

    [feat] Update Model Card Model

--- back-end/src/models/model.py
+++ back-end/src/models/model.py
@@ -1,48 +1,31 @@
+from datetime import datetime
 from typing import Dict, List, Optional
 
 from bson import ObjectId
-from pydantic import BaseModel, Field
+from pydantic import AnyUrl, BaseModel, Field
 
 from .common import PyObjectId
-from .engine import InferenceEngine
-
-
-class SectionModel(BaseModel):
-    text: str
-    title: str
-    media: Optional[List[Dict]]
 
 
 class ModelCardModelIn(BaseModel):  # Input spec
     model_id: str  # unique id
     title: str
-    # NOTE: flattened sections to make schema easier
-    # description: Dict[SectionTypes, Section]
-    description: SectionModel
-    limitations: SectionModel
-    metrics: SectionModel
-    explanation: SectionModel
-    deployment: SectionModel
-    performance: Optional[SectionModel]
-    model_details: Optional[
-        SectionModel
-    ]  # store model genre, format and framework
-    datetime: str
+    description: str
+    performance: str
+    created: datetime
+    last_modified: datetime
     tags: List[str]  # for all other tags
     task: str  # a task is a tag
-    frameworks: List[
-        str
-    ]  # TODO: decide if this should be a singular tag or allow multiple
-    point_of_contact: str
-    creator: Optional[str]
-    # TODO: Figure out model source stuff
+    frameworks: List[str]
+    point_of_contact: Optional[str]
+    owner: Optional[str]
     clearml_exp_id: Optional[str]
-    inference_engine: Optional[InferenceEngine]
+    inference_api: AnyUrl
 
 
 class ModelCardModelDB(ModelCardModelIn):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    owner_id: str  # to be dynamically put in by FastAPI
+    creator_user_id: str  # to be dynamically put in by FastAPI
 
     class Config:
         allow_population_by_field_name = True
@@ -52,36 +35,29 @@ class ModelCardModelDB(ModelCardModelIn):
 
 class FindModelCardModel(BaseModel):
     model_id: Optional[str]
-    owner_id: Optional[str]
+    creator_user_id: Optional[str]
     title: Optional[str]
     tags: Optional[List[str]]
     task: Optional[str]
     frameworks: Optional[List[str]]
     point_of_contact: Optional[str]
-    creator: Optional[str]
+    owner: Optional[str]
     sort: Optional[List[List[str]]]  # [(sort col, sort direction)]
     return_attrs: Optional[List[str]]
 
 
 class UpdateModelCardModel(BaseModel):
+    model_id: Optional[str]  # unique id
     title: Optional[str]
-    description: Optional[SectionModel]
-    limitations: Optional[SectionModel]
-    metrics: Optional[SectionModel]
-    explanation: Optional[SectionModel]
-    deployment: Optional[SectionModel]
-    performance: Optional[SectionModel]
-    model_details: Optional[SectionModel]
-    datetime: Optional[str]
-    tags: Optional[List[str]]
-    task: Optional[str]
-    frameworks: Optional[
-        List[str]
-    ]  # NOTE: tbd if this should be just 1 string
+    description: Optional[str]
+    performance: Optional[str]
+    tags: Optional[List[str]]  # for all other tags
+    task: Optional[str]  # a task is a tag
+    frameworks: Optional[List[str]]
     point_of_contact: Optional[str]
-    creator: Optional[str]
+    owner: Optional[str]
     clearml_exp_id: Optional[str]
-    inference_engine: Optional[InferenceEngine]
+    inference_api: Optional[AnyUrl]
 
     class Config:
         arbitrary_types_allowed = True
--- front-end/src/stores/model-store.ts
+++ front-end/src/stores/model-store.ts
@@ -2,23 +2,45 @@ import { api } from 'src/boot/axios';
 import { defineStore } from 'pinia';
 
 export interface ModelCard {
-  id: string;
+  modelId: string;
+  title: string;
+  task: string;
+  tags: string[];
+  creator: string;
+  owner?: string;
+  pointOfContact?: string;
+  inferenceApi: string;
+  description: string;
+  performance: string;
+  created: Date;
+  lastModified: Date;
 }
 
 export const useModelStore = defineStore('model', {
-  state: () => ({
-    models: [],
-  }),
-  getters: {
-    userModels(state) {
-      return state.models;
-    },
-  },
+  state: () => ({}),
+  getters: {},
   actions: {
-    getModelById(id: string): ModelCard {
-      return {
-        id: id,
+    async getModelById(id: string): Promise<ModelCard> {
+      const res = await api.post(`models/${id}`);
+      if (res.status !== 200) {
+        console.error('TODO: Add exception to throw');
+      }
+      const data = res.data;
+      const model = {
+        modelId: data.model_id as string,
+        title: data.title as string,
+        task: data.task as string,
+        tags: ((data.tags as string[]) + data.frameworks) as string[],
+        creator: data.creator as string,
+        owner: data.owner as string,
+        pointOfContact: data.point_of_contact as string,
+        inferenceApi: data.inference_engine.service_url as string,
+        description: data.description as string,
+        performance: data.performance as string,
+        created: data.created as Date,
+        lastModified: data.last_modified as Date,
       } as ModelCard;
+      return model;
     },
     async getAll(): Promise<ModelCard[]> {
       const res = await api.post('models/search', {});
