8f7af961be2d1aa87437f562b010f292579d8334
Tien-Cheng <tiencheng@protonmail.com>
2022-10-27 11:01:34 +0800

    [refactor] Refactor route to model to use new composite index

--- front-end/src/router/routes.ts
+++ front-end/src/router/routes.ts
@@ -24,7 +24,7 @@ const routes: RouteRecordRaw[] = [
     component: MainLayout,
     children: [
       {
-        path: ':id',
+        path: ':userId/:modelId',
         component: ModelPage,
       },
       {
