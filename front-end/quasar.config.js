95f2c4c18ca11c759aad88e77351d2b28594f52e
Tien-Cheng <tiencheng@protonmail.com>
2022-10-27 11:12:31 +0800

    [feat] Configure loading bar in Quasar

--- front-end/quasar.config.js
+++ front-end/quasar.config.js
@@ -85,7 +85,13 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
-      config: {},
+      config: {
+        loadingBar: {
+          color: 'primary',
+          size: '15px',
+          position: 'bottom',
+        },
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -98,7 +104,7 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['Cookies'],
+      plugins: ['Cookies', 'LoadingBar'],
     },
 
     // animations: 'all', // --- includes all animations
