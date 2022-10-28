4199fa313bf0de9c7dda91bcc71f98afc49c4e08
Tien-Cheng <tiencheng@protonmail.com>
2022-10-28 10:01:02 +0800

    [fix] Attempt to refresh access token on 401 error as well

--- front-end/src/boot/axios.ts
+++ front-end/src/boot/axios.ts
@@ -34,7 +34,7 @@ api.interceptors.response.use(
   },
   async (error) => {
     const originalRequest = error.config;
-    if (error.response?.status === 403 && !originalRequest._retry) {
+    if ((error.response?.status === 403 || error.response?.status === 401) && !originalRequest._retry) {
       originalRequest._retry = true;
       // get refresh token
       const authStore = useAuthStore();
