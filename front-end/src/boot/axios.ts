49db3f816135cc9af64e6a09c8c13c81e4d5c89f
Mathias Ho <osirishyp@gmail.com>
2022-11-01 13:10:37 +0800

    url issues with cors

--- front-end/src/boot/axios.ts
+++ front-end/src/boot/axios.ts
@@ -17,7 +17,7 @@ declare module '@vue/runtime-core' {
 // "export default () => {}" function below (which runs individually
 // for each client)
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:7070',
+  baseURL: 'http://localhost:7070',
   withCredentials: true,
 });
 
