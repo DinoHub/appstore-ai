7e7648290a2722d88cc551f05d5ee5d89d0f3f91
Tien-Cheng <tiencheng@protonmail.com>
2022-11-03 18:00:58 +0800

    [wip] Attempt to improve look of dashboard

--- front-end/src/pages/DashboardPage.vue
+++ front-end/src/pages/DashboardPage.vue
@@ -11,12 +11,16 @@
           card-class="col-xs-12 col-sm-6 col-md-3 col-xl-4"
         >
           <template v-slot:top-left>
-            <div class="col col-sm-3 text-h4">
-              Your Models
-              <q-btn round icon="add" to="/models/create"></q-btn>
-              <router-link class="router-link text-body2" to="/models"
-                >View all models</router-link
-              >
+            <div class="row content-stretch">
+              <div class="col col-sm-5 text-h4">Your Models</div>
+              <div class="col col-sm-2">
+                <q-btn round icon="add" to="/models/create"></q-btn>
+              </div>
+              <div class="col col-sm-5">
+                <router-link class="text-body2" to="/models"
+                  >View all models</router-link
+                >
+              </div>
             </div>
           </template>
         </model-card-data-table>
