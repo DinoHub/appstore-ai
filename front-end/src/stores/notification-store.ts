accbdf72e4fbcc2dba8a06a245d79424df011fbb
Tien-Cheng <tiencheng@protonmail.com>
2022-11-09 17:11:56 +0800

    [wip] Add Notification interface

--- front-end/src/stores/notification-store.ts
+++ front-end/src/stores/notification-store.ts
@@ -1,14 +1,22 @@
-import { QNotifyOptions } from 'quasar';
 import { defineStore } from 'pinia';
+
+export enum Severity {
+  default = 'default',
+  info = 'info',
+  warning = 'warning',
+  danger = 'danger',
+}
+export interface Notification {
+  message: string;
+  severity: Severity;
+}
 
 export const useNotificationStore = defineStore('notification', {
   state: () => ({
-    notifications: [] as QNotifyOptions[],
+    notifications: [] as Notification[],
   }),
 
-  getters: {
-  },
+  getters: {},
 
-  actions: {
-  }
+  actions: {},
 });
