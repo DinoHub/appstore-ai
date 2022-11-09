4eceace72fe9acd1611aa34d287cf8c089cce240
Tien-Cheng <tiencheng@protonmail.com>
2022-11-09 17:11:46 +0800

    [feat] Floating and bubble menu for editor

--- front-end/src/components/TiptapEditor.vue
+++ front-end/src/components/TiptapEditor.vue
@@ -1,7 +1,17 @@
+<style scoped>
+.sticky {
+  position: sticky;
+  top: 0;
+}
+</style>
 <template>
   <!-- Editor Toolbar -->
-  <div v-if="editor">
-    <q-bar class="bg-white">
+  <div v-if="editor" style="overflow: none">
+    <bubble-menu
+      class="q-gutter-xs row"
+      :tippy-options="{ duration: 180 }"
+      :editor="editor"
+    >
       <!-- Bold -->
       <q-btn
         dense
@@ -49,7 +59,7 @@
           _iconFill(editor?.isActive('heading', { level: 1 }) ?? true)
         "
         :color="_buttonBg(editor?.isActive('heading', { level: 1 }) ?? true)"
-        icon="looks_one"
+        label="H1"
         @click="editor?.chain().focus().setHeading({ level: 1 }).run()"
       />
       <q-btn
@@ -58,7 +68,7 @@
           _iconFill(editor?.isActive('heading', { level: 2 }) ?? true)
         "
         :color="_buttonBg(editor?.isActive('heading', { level: 2 }) ?? true)"
-        icon="looks_two"
+        label="H2"
         @click="editor?.chain().focus().setHeading({ level: 2 }).run()"
       />
       <q-btn
@@ -67,7 +77,153 @@
           _iconFill(editor?.isActive('heading', { level: 3 }) ?? true)
         "
         :color="_buttonBg(editor?.isActive('heading', { level: 3 }) ?? true)"
-        icon="looks_3"
+        label="H3"
+        @click="editor?.chain().focus().setHeading({ level: 3 }).run()"
+      />
+      <!-- Bullet List -->
+      <q-btn
+        dense
+        :text-color="_iconFill(editor?.isActive('bulletList') ?? true)"
+        :color="_buttonBg(editor?.isActive('bulletList') ?? true)"
+        icon="format_list_bulleted"
+        @click="editor?.chain().focus().toggleBulletList().run()"
+      />
+      <!-- Ordered List -->
+      <q-btn
+        dense
+        :text-color="_iconFill(editor?.isActive('orderedList') ?? true)"
+        :color="_buttonBg(editor?.isActive('orderedList') ?? true)"
+        icon="format_list_numbered"
+        @click="editor?.chain().focus().toggleOrderedList().run()"
+      />
+    </bubble-menu>
+    <floating-menu
+      class="q-gutter-xs row"
+      :tippy-options="{ duration: 100 }"
+      :editor="editor"
+    >
+      <!-- H1-H3 -->
+      <q-btn
+        dense
+        :text-color="
+          _iconFill(editor?.isActive('heading', { level: 1 }) ?? true)
+        "
+        :color="_buttonBg(editor?.isActive('heading', { level: 1 }) ?? true)"
+        label="H1"
+        @click="editor?.chain().focus().setHeading({ level: 1 }).run()"
+      />
+      <q-btn
+        dense
+        :text-color="
+          _iconFill(editor?.isActive('heading', { level: 2 }) ?? true)
+        "
+        :color="_buttonBg(editor?.isActive('heading', { level: 2 }) ?? true)"
+        label="H2"
+        @click="editor?.chain().focus().setHeading({ level: 2 }).run()"
+      />
+      <q-btn
+        dense
+        :text-color="
+          _iconFill(editor?.isActive('heading', { level: 3 }) ?? true)
+        "
+        :color="_buttonBg(editor?.isActive('heading', { level: 3 }) ?? true)"
+        label="H3"
+        @click="editor?.chain().focus().setHeading({ level: 3 }).run()"
+      />
+      <!-- Code Block -->
+      <q-btn
+        dense
+        :text-color="_iconFill(editor?.isActive('codeBlock') ?? true)"
+        :color="_buttonBg(editor?.isActive('codeBlock') ?? true)"
+        icon="code"
+        @click="editor?.chain().focus().toggleCodeBlock().run()"
+      />
+      <!-- Bullet List -->
+      <q-btn
+        dense
+        :text-color="_iconFill(editor?.isActive('bulletList') ?? true)"
+        :color="_buttonBg(editor?.isActive('bulletList') ?? true)"
+        icon="format_list_bulleted"
+        @click="editor?.chain().focus().toggleBulletList().run()"
+      />
+      <!-- Ordered List -->
+      <q-btn
+        dense
+        :text-color="_iconFill(editor?.isActive('orderedList') ?? true)"
+        :color="_buttonBg(editor?.isActive('orderedList') ?? true)"
+        icon="format_list_numbered"
+        @click="editor?.chain().focus().toggleOrderedList().run()"
+      />
+      <!-- Chart Editor -->
+      <q-btn dense icon="insert_chart" @click="chartEditor = true" />
+    </floating-menu>
+    <aside class="bg-white q-gutter-sm row">
+      <!-- Bold -->
+      <q-btn
+        dense
+        :text-color="_iconFill(editor?.isActive('bold') ?? true)"
+        :color="_buttonBg(editor?.isActive('bold') ?? true)"
+        icon="format_bold"
+        @click="editor?.chain().focus().toggleBold().run()"
+      />
+      <!-- Italic -->
+      <q-btn
+        dense
+        :text-color="_iconFill(editor?.isActive('italic') ?? true)"
+        :color="_buttonBg(editor?.isActive('italic') ?? true)"
+        icon="format_italic"
+        @click="editor?.chain().focus().toggleItalic().run()"
+      />
+      <!-- Underline -->
+      <q-btn
+        dense
+        :text-color="_iconFill(editor?.isActive('underline') ?? true)"
+        :color="_buttonBg(editor?.isActive('underline') ?? true)"
+        icon="format_underline"
+        @click="editor?.chain().focus().toggleUnderline().run()"
+      />
+      <!-- Strike -->
+      <q-btn
+        dense
+        :text-color="_iconFill(editor?.isActive('strike') ?? true)"
+        :color="_buttonBg(editor?.isActive('strike') ?? true)"
+        icon="format_strikethrough"
+        @click="editor?.chain().focus().toggleStrike().run()"
+      />
+      <!-- Code Block -->
+      <q-btn
+        dense
+        :text-color="_iconFill(editor?.isActive('codeBlock') ?? true)"
+        :color="_buttonBg(editor?.isActive('codeBlock') ?? true)"
+        icon="code"
+        @click="editor?.chain().focus().toggleCodeBlock().run()"
+      />
+      <!-- H1-H3 -->
+      <q-btn
+        dense
+        :text-color="
+          _iconFill(editor?.isActive('heading', { level: 1 }) ?? true)
+        "
+        :color="_buttonBg(editor?.isActive('heading', { level: 1 }) ?? true)"
+        label="H1"
+        @click="editor?.chain().focus().setHeading({ level: 1 }).run()"
+      />
+      <q-btn
+        dense
+        :text-color="
+          _iconFill(editor?.isActive('heading', { level: 2 }) ?? true)
+        "
+        :color="_buttonBg(editor?.isActive('heading', { level: 2 }) ?? true)"
+        label="H2"
+        @click="editor?.chain().focus().setHeading({ level: 2 }).run()"
+      />
+      <q-btn
+        dense
+        :text-color="
+          _iconFill(editor?.isActive('heading', { level: 3 }) ?? true)
+        "
+        :color="_buttonBg(editor?.isActive('heading', { level: 3 }) ?? true)"
+        label="H3"
         @click="editor?.chain().focus().setHeading({ level: 3 }).run()"
       />
       <!-- Bullet List -->
@@ -94,21 +250,28 @@
         ></plotly-editor>
       </q-dialog>
       <!-- Show Source Code -->
-      <q-btn label="Show Source Code" dense @click="showSource = true"></q-btn>
+      <q-btn
+        label="Show Source Code"
+        dense
+        @click="
+          () => {
+            displaySourceCode(editor);
+          }
+        "
+      ></q-btn>
       <q-dialog v-model="showSource">
         <q-card>
           <q-card-section>
             <pre>
-              <code class="language-html">
-            {{ editor?.getHTML() }}
-              </code>
-            </pre>
+                <code id="source" class="language-html">
+                </code>
+              </pre>
           </q-card-section>
         </q-card>
       </q-dialog>
-    </q-bar>
+    </aside>
     <editor-content
-      class="text-left"
+      class="text-left q-pl-md"
       @click="editor?.commands.focus()"
       :editor="editor"
     />
@@ -116,13 +279,26 @@
 </template>
 
 <script setup lang="ts">
-import { useEditor, EditorContent } from '@tiptap/vue-3';
+import {
+  useEditor,
+  EditorContent,
+  BubbleMenu,
+  FloatingMenu,
+} from '@tiptap/vue-3';
+
 import PlotlyEditor from './PlotlyEditor.vue';
+
 import StarterKit from '@tiptap/starter-kit';
 import Underline from '@tiptap/extension-underline';
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
 import Placeholder from '@tiptap/extension-placeholder';
+import Table from '@tiptap/extension-table';
+import TableHeader from '@tiptap/extension-table-header';
+import TableRow from '@tiptap/extension-table-row';
+import TableCell from '@tiptap/extension-table-cell';
 import Chart from 'src/plugins/tiptap-charts';
+
+import hljs from 'highlight.js';
 import css from 'highlight.js/lib/languages/css';
 import js from 'highlight.js/lib/languages/javascript';
 import ts from 'highlight.js/lib/languages/typescript';
@@ -144,7 +320,6 @@ export interface Props {
 }
 
 const props = defineProps<Props>();
-
 const editor = useEditor({
   extensions: [
     Chart,
@@ -154,6 +329,12 @@ const editor = useEditor({
     CodeBlockLowlight.configure({
       lowlight,
     }),
+    Table.configure({
+      resizable: true,
+    }),
+    TableRow,
+    TableHeader,
+    TableCell,
   ],
   content: props.content ?? 'Type here...',
 });
@@ -185,6 +366,19 @@ function insertChart(
       },
     })
     .run();
+}
+
+function displaySourceCode(editor: Editor) {
+  // TODO: FIX THIS
+  showSource.value = true;
+  const select = document.getElementById('source');
+  if (select) {
+    select.innerHTML = editor.getHTML();
+    console.log(select.innerHTML);
+    hljs.highlightAll();
+  } else {
+    console.warn('Cannot find!');
+  }
 }
 </script>
 <style lang="scss">
@@ -260,5 +454,71 @@ function insertChart(
       font-weight: 700;
     }
   }
+}
+
+.ProseMirror {
+  table {
+    border-collapse: collapse;
+    table-layout: fixed;
+    width: 100%;
+    margin: 0;
+    overflow: hidden;
+
+    td,
+    th {
+      min-width: 1em;
+      border: 2px solid #ced4da;
+      padding: 3px 5px;
+      vertical-align: top;
+      box-sizing: border-box;
+      position: relative;
+
+      > * {
+        margin-bottom: 0;
+      }
+    }
+
+    th {
+      font-weight: bold;
+      text-align: left;
+      background-color: #f1f3f5;
+    }
+
+    .selectedCell:after {
+      z-index: 2;
+      position: absolute;
+      content: '';
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      background: rgba(200, 200, 255, 0.4);
+      pointer-events: none;
+    }
+
+    .column-resize-handle {
+      position: absolute;
+      right: -2px;
+      top: 0;
+      bottom: -2px;
+      width: 4px;
+      background-color: #adf;
+      pointer-events: none;
+    }
+
+    p {
+      margin: 0;
+    }
+  }
+}
+
+.tableWrapper {
+  padding: 1rem 0;
+  overflow-x: auto;
+}
+
+.resize-cursor {
+  cursor: ew-resize;
+  cursor: col-resize;
 }
 </style>
