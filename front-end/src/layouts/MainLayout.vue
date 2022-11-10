5fc4236235df66e7b2eeec045ad26fdf001fca3c
Tien-Cheng <tiencheng@protonmail.com>
2022-11-10 16:05:14 +0800

    [fix] Add breadcrumb

--- front-end/src/layouts/MainLayout.vue
+++ front-end/src/layouts/MainLayout.vue
@@ -4,6 +4,7 @@
       <the-navbar></the-navbar>
     </q-header>
     <q-page-container>
+      <route-breadcrumbs class="q-pt-md q-pl-md"></route-breadcrumbs>
       <router-view />
     </q-page-container>
     <q-footer>
@@ -15,4 +16,5 @@
 <script setup lang="ts">
 import TheFooter from 'src/components/TheFooter.vue';
 import TheNavbar from 'src/components/TheNavbar.vue';
+import RouteBreadcrumbs from 'src/components/RouteBreadcrumbs.vue';
 </script>
