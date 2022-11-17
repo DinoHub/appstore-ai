a9c82b4c940a718cd201b5e8dd15e2b41c555868
Tien-Cheng <tiencheng@protonmail.com>
2022-11-17 09:58:46 +0800

    [config] Set up script for Kind registry

--- quick-deploy.sh
+++ quick-deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
+# create registry container unless it already exists
+reg_name='kind-registry'
+reg_port='5001'
+if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
+    registry:2
+fi
 kind create cluster --config=k8s/kind.yaml
+
+if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
+  docker network connect "kind" "${reg_name}"
+fi
+
+kubectl apply -f k8s/registry.yaml
+
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/aio/deploy/recommended.yaml
 kubectl apply -f k8s/dashboard/admin.yaml
 sh k8s/dashboard/generate-token.sh
@@ -9,6 +24,7 @@ helm repo add metallb https://metallb.github.io/metallb
 helm install metallb metallb/metallb -n metallb-system
 kubectl wait --for=condition=Ready pod --all -n metallb-system
 # Duplicate as sometimes above times out just as it finishes
+sleep 5
 kubectl wait --for=condition=Ready pod --all -n metallb-system
 kubectl apply -f k8s/metallb-config.yaml -n metallb-system
 
