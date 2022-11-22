5f915d46d3f0d79e3c6b9f1f2983578fe4ceee81
Tien-Cheng <tiencheng@protonmail.com>
2022-11-22 15:29:29 +0800

    [config] Revert config of api url

--- front-end/quasar.config.js
+++ front-end/quasar.config.js
@@ -64,7 +64,7 @@ module.exports = configure(function (ctx) {
       // analyze: true,
       env: {
         backendAPI: ctx.dev
-          ? 'http://127.0.0.1:8080'
+          ? 'http://localhost:7070'
           : 'http://api.appstore.ai',
       },
       // rawDefine: {}
