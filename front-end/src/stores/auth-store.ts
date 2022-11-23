22eefbba41673d7b64c3999c5aa92b5db729cb17
Mathias Ho <osirishyp@gmail.com>
2022-11-23 09:04:12 +0800

    [update] added new actions for admin login and logout for store

--- front-end/src/stores/auth-store.ts
+++ front-end/src/stores/auth-store.ts
@@ -1,8 +1,7 @@
 import { api } from 'src/boot/axios';
 import { defineStore } from 'pinia';
 import jwt_decode from 'jwt-decode';
-import { Cookies, useQuasar, Notify } from 'quasar';
-
+import { Notify } from 'quasar';
 export enum Role {
   user = 'user',
   admin = 'admin',
@@ -60,15 +59,62 @@ export const useAuthStore = defineStore('auth', {
           message: 'Failed to login',
         });
       }
+      if (this.returnUrl == '/admin') {
+        this.returnUrl = '/';
+      }
       this.router.push(this.returnUrl || '/');
+    },
+    async admin_login(userId: string, password: string): Promise<void> {
+      try {
+        const creds = new FormData();
+        creds.append('username', userId);
+        creds.append('password', password);
+        const response = await api.post('/auth/', creds);
+        const admin_check = await api.get('/auth/is_admin');
+        // Decode JWT
+        const { access_token }: LoginResponse = response.data;
+        const jwt_data = jwt_decode(access_token) as JWT;
+        if (!jwt_data) {
+          console.error('Failed to decode');
+        }
+
+        localStorage.removeItem('creationStore');
+        this.user = {
+          // TODO: Replace with actual api call
+          userId: jwt_data.sub,
+          name: jwt_data.name,
+          role: jwt_data.role,
+        } as User;
+        this.router.push('/admin');
+      } catch (err) {
+        Notify.create({
+          type: 'negative',
+          position: 'top',
+          message:
+            'Failed to login, credentials invalid or insufficient privileges',
+        });
+      }
     },
     logout(): void {
       try {
         api.delete('/auth/logout');
         this.user = null;
+        this.returnUrl = this.router.currentRoute.value.fullPath;
         localStorage.removeItem('auth');
         localStorage.removeItem('creationStore');
         this.router.push({ name: 'Login' });
+      } catch (err) {
+        console.warn('Logout failed');
+        console.warn(err);
+      }
+    },
+    admin_logout(): void {
+      try {
+        api.delete('/auth/logout');
+        this.user = null;
+        localStorage.removeItem('auth');
+        localStorage.removeItem('creationStore');
+        this.router.push({ name: 'adminLogin' });
       } catch (err) {
         console.warn('Logout failed');
       }
