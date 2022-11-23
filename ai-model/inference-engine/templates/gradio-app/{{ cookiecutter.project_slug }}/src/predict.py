dbeaf44ea9d8d860552f56ddeea2e9a56f4fb4c5
Tien-Cheng <tiencheng@protonmail.com>
2022-11-23 16:37:03 +0800

    [doc] Add guide to building Gradio app

--- ai-model/inference-engine/templates/gradio-app/{{ cookiecutter.project_slug }}/src/predict.py
+++ ai-model/inference-engine/templates/gradio-app/{{ cookiecutter.project_slug }}/src/predict.py
@@ -4,10 +4,11 @@ from typing import Any, List, Optional, Union
 import gradio as gr
 from config import config
 
-inputs: List[Union[str, gr.components.Component]] = []
-outputs: List[Union[str, gr.components.Component]] = []
+inputs: List[Union[str, gr.components.Component]] = ["text"]
+outputs: List[Union[str, gr.components.Component]] = ["text"]
 examples: Optional[Union[List[Any], List[List[Any]], str]] = None
 
 
-def predict():
-    raise NotImplementedError
+def predict(name: str) -> str:
+    # TODO: Implement this!
+    return f"Hello {name}"
