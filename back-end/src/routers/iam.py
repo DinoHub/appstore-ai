4b662ca7c34f9bb76ba1545931fc9d6773aa7baa
Mathias Ho <osirishyp@gmail.com>
2022-11-30 17:32:05 +0800

    [update] modified user delete endpoint to use UserRemoval model + new endpoint to bulk edit users to change permissions of users + modified paginated retrieval endpoint to be able to search by userId, search by range of last modified date & created date

--- back-end/src/routers/iam.py
+++ back-end/src/routers/iam.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Any
 import datetime
 from fastapi import APIRouter, Depends, HTTPException, Path, status, Query
 from fastapi.responses import JSONResponse, Response
@@ -7,7 +7,7 @@ from pymongo import ASCENDING, DESCENDING
 
 from ..internal.auth import check_is_admin, get_password_hash
 from ..internal.db import get_db
-from ..models.iam import UserInsert, UserPage
+from ..models.iam import UserInsert, UserPage, UserRemoval, UsersEdit
 
 # use openssl rand -hex 32 to generate secret key
 ACCESS_TOKEN_EXPIRE_MINUTES = 45
@@ -68,14 +68,14 @@ async def add_user(
 
 @router.delete("/delete", dependencies=[Depends(check_is_admin)])
 async def delete_user(
-    userid: List[str],
+    userid: UserRemoval,
     db=Depends(get_db),
 ):
     db, mongo_client = db
     try:
         async with await mongo_client.start_session() as session:
             async with session.start_transaction():
-                await db["users"].delete_many({"userId": {"$in": userid}})
+                await db["users"].delete_many({"userId": {"$in": userid.users}})
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     except:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Not found")
@@ -112,6 +112,38 @@ async def update_user(
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
+@router.put("/edit/multi", dependencies=[Depends(check_is_admin)])
+async def update_many_user(
+    user: UsersEdit,
+    db=Depends(get_db),
+):
+    db, mongo_client = db
+    try:
+        if user.priv == None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Privilege must be set properly",
+            )
+        async with await mongo_client.start_session() as session:
+            async with session.start_transaction():
+                await db["users"].update_many(
+                    {"userId": {"$in": user.users}},
+                    {
+                        "$set": {
+                            "adminPriv": user.priv,
+                            "lastModified": str(datetime.datetime.now()),
+                        }
+                    },
+                )
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=f""
+        )
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Not found")
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
 # list users with pagination
 @router.post("/", dependencies=[Depends(check_is_admin)])
 async def get_users(
@@ -124,16 +156,37 @@ async def get_users(
     try:
         # check number of documents to skip past
         skips = pages_user.user_num * (pages_user.page_num - 1)
-        # lookups for name and admin priv matching
+        # dictionary for lookups
         lookup = {}
+        # narrow search by users that include given name from request if not none
         if pages_user.name != None:
             lookup["name"] = {"$regex": pages_user.name, "$options": "i"}
+        # narrow search by users that include given userId from request if not none
+        if pages_user.userId != None:
+            lookup["userId"] = {"$regex": pages_user.userId, "$options": "i"}
+        # narrow search by looking for users with/without admin priv only based on req
         if pages_user.admin_priv != None:
             lookup["adminPriv"] = pages_user.admin_priv
-        # dont skip if 1st page
+        # narrow search by looking for users last modified within a date range given by req
+        if pages_user.last_modified_range != None:
+            if type(pages_user.last_modified_range) is dict:
+                lookup["lastModified"] = {
+                    "$gte": pages_user.last_modified_range["from"],
+                    "$lte": pages_user.last_modified_range["to"],
+                }
+        # narrow search by looking for users created within a date range given by req
+        if pages_user.date_created_range != None:
+            if type(pages_user.date_created_range) is dict:
+                lookup["created"] = {
+                    "$gte": pages_user.date_created_range["from"],
+                    "$lte": pages_user.date_created_range["to"],
+                }
+        # start session + transaction to get data from MongoDB
         async with await mongo_client.start_session() as session:
             async with session.start_transaction():
+                # check for the total number of rows in 'users' collection
                 total_rows = await (db["users"].count_documents(lookup))
+                # dont skip if 1st page
                 if skips <= 0:
                     # find from users in MongodDB exclude ObjectID and convert to list
                     cursor = await (
@@ -152,15 +205,16 @@ async def get_users(
                         .skip(skips)
                         .limit(pages_user.user_num)
                     ).to_list(length=pages_user.user_num)
-                # return documents
-
+        # return documents if all ok
         return JSONResponse(
             status_code=status.HTTP_200_OK,
             content={"results": cursor, "total_rows": total_rows},
         )
+    # triggered if req sent to this endpoint has missing headers or invalid data
     except ValueError:
         return JSONResponse(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, content=f""
         )
+    # triggered if all else fails
     except Exception:
         return HTTPException(status_code=status.HTTP_404_NOT_FOUND)
