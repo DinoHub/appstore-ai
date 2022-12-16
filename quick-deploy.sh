74c33f56ad2730de3a0bf48d6d52c2098968de81
Tien-Cheng <tiencheng@protonmail.com>
2022-12-16 15:21:22 +0800

    [refactor] Clean up deploy scripts

--- quick-deploy.sh
+++ quick-deploy.sh
@@ -17,15 +17,15 @@ fi
 # kubectl apply -f k8s/registry.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/aio/deploy/recommended.yaml
-kubectl apply -f k8s/dashboard/admin.yaml
-sh k8s/dashboard/generate-token.sh
+kubectl apply -f k8s/dev/dashboard/admin.yaml
+sh k8s/dev/dashboard/generate-token.sh
 # Set up MetalLB
 kubectl create namespace metallb-system
 helm repo add metallb https://metallb.github.io/metallb
 helm install metallb metallb/metallb -n metallb-system
 kubectl wait --for=condition=Ready pod --all -n metallb-system
 sleep 20
-kubectl apply -f k8s/metallb-config.yaml -n metallb-system
+kubectl apply -f k8s/dev/metallb-config.yaml -n metallb-system
 
 # Setup NGINX Ingress
 # Note the below is Kind specific for now
@@ -35,14 +35,15 @@ kubectl apply -f k8s/metallb-config.yaml -n metallb-system
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
 # Set up KNative
-kubectl apply -f https://github.com/knative/operator/releases/download/knative-v1.7.1/operator.yaml
-kubectl create namespace knative-serving
+kubectl apply -f https://github.com/knative/operator/releases/download/knative-v1.8.1/operator.yaml
+# kubectl create namespace knative-serving
 
 # Load in images
-kind load docker-image aas-back-end:latest
-kind load docker-image aas-front-end:latest
+kind load docker-image aas-backend:latest
+kind load docker-image aas-frontend:latest
 # Set up helm charts
-helm install ai-mongodb charts/mongodb/ --values charts/mongodb/values.yaml
-helm install ai-backend charts/ai-be/ --values charts/ai-be/values.yaml
-helm install ai-frontend charts/ai-fe/ --values charts/ai-fe/values.yaml
-helm install inference-engine charts/ai-ie/ --values charts/ai-be/values.yaml  --create-namespace --namespace inference-engine
+helm install appstore k8s/charts/appstore
+# helm install ai-mongodb charts/mongodb/ --values charts/mongodb/values.yaml
+# helm install ai-backend charts/ai-be/ --values charts/ai-be/values.yaml
+# helm install ai-frontend charts/ai-fe/ --values charts/ai-fe/values.yaml
+# helm install inference-engine charts/ai-ie/ --values charts/ai-be/values.yaml  --create-namespace --namespace inference-engine
