5b0b7dbe53704917a8aaae044ba905f54d7425dd
Tien-Cheng <tiencheng@protonmail.com>
2022-12-16 14:59:58 +0800

    [fix] Fix mongodb dependency not installing in helm chart

--- k8s/dev/install-prereqs.sh
+++ k8s/dev/install-prereqs.sh
@@ -5,6 +5,7 @@ sh dashboard/generate-token.sh
 # Set up MetalLB
 kubectl create namespace metallb-system
 helm repo add metallb https://metallb.github.io/metallb
+helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install metallb metallb/metallb -n metallb-system
 kubectl wait --for=condition=Ready pod --all -n metallb-system
 kubectl apply -f metallb-config.yaml -n metallb-system
@@ -16,6 +17,8 @@ kubectl create namespace knative-serving
 kubectl create namespace inference-engine
 # kubectl apply -f knative/knative-serving.yaml
 
+kind load docker-image aas-backend:latest
+kind load docker-image aas-frontend:latest
 # Set up Magic DNS
 # kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.7.2/serving-default-domain.yaml
 # Make KNative Services Private (only allow within cluster call)
