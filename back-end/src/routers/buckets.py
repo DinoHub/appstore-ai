42a0f3b7030c9f0b90e2a0e9b0b9e3036e69b2fd
Mathias Ho <osirishyp@gmail.com>
2022-12-29 14:09:57 +0800

    [update] PUT route for replacing video related to model card and make sure video name is randomly generated using UUID4 with correct file extension

--- back-end/src/routers/buckets.py
+++ back-end/src/routers/buckets.py
@@ -1,13 +1,14 @@
 import io
+import uuid
 
 from colorama import Fore
-from fastapi import APIRouter, Depends, HTTPException, UploadFile, status
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, status, Form, File
 from fastapi.responses import JSONResponse
 from minio import Minio
 
 from ..config.config import config
 from ..internal.auth import get_current_user
-from ..internal.minio_client import minio_api_client, upload_data
+from ..internal.minio_client import minio_api_client, upload_data, remove_data
 from ..models.iam import TokenData
 
 router = APIRouter(prefix="/buckets", tags=["buckets"])
@@ -28,7 +29,7 @@ def upload_video(
             path = upload_data(
                 s3_client,
                 video.file.read(),
-                f"videos/{video.filename}",
+                f"videos/{uuid.uuid4().hex}.{video.content_type.replace('video/','')}",
                 BUCKET_NAME,
                 video.content_type,
             )
@@ -50,6 +51,48 @@ def upload_video(
         return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content=f"Something went wrong the upload",
+        )
+
+
+@router.put("/video", status_code=status.HTTP_200_OK)
+def replace_video(
+    new_video: UploadFile = Form(),
+    old_video_location: str = Form(),
+    user: TokenData = Depends(get_current_user),
+    s3_client: Minio = Depends(minio_api_client),
+):
+    try:
+        if not user.user_id:
+            return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+        if "video" in new_video.content_type:
+
+            # remove the old video inside the bucket
+            video_location = old_video_location.replace(
+                f"{config.MINIO_API_HOST}/{config.MINIO_BUCKET_NAME}/", ""
+            )
+            remove_data(s3_client, video_location, config.MINIO_BUCKET_NAME)
+
+            # upload the new video to the bucket
+            path = upload_data(
+                s3_client,
+                new_video.file.read(),
+                f"videos/{uuid.uuid4().hex}.{new_video.content_type.replace('video/','')}",
+                BUCKET_NAME,
+                new_video.content_type,
+            )
+            return {"video_location": path}
+
+        # return this if new file is not video
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content=f"Something went wrong the removal of the video",
+        )
+    except Exception as e:
+        print(f"{Fore.RED}ERROR{Fore.WHITE}:\t  {e}")
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content=f"Something went wrong the removal of the video",
         )
 
 
