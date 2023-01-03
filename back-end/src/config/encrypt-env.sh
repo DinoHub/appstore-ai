be83acac7073397903dec05270dfa3d12e53b97d
Tien-Cheng <tiencheng@protonmail.com>
2023-01-03 09:30:20 +0800

    [fix] Fix gpg encryption of passphrase

--- back-end/src/config/encrypt-env.sh
+++ back-end/src/config/encrypt-env.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Encrypting environment variables with passphrase"
-gpg --symmetric --cipher-algo AES256 --batch --passphrase $1 --output .env.gpg .env
+gpg --symmetric --cipher-algo AES256 --batch --passphrase "$1" --output .env.gpg .env
