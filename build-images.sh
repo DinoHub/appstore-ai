6537fb8c9fc312e1de097fe3cf5af01716630312
Tien-Cheng <tiencheng@protonmail.com>
2023-01-06 12:35:46 +0800

    [ver] Bump image version to 1.0.1

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Build image
-docker build back-end -t aas-backend:1.0.0 --build-arg ENV_STATE=prod
+docker build back-end -t aas-backend:1.0.1 --build-arg ENV_STATE=prod
 # docker push localhost:5001/aas-back-end:latest
-docker build front-end -t aas-frontend:1.0.0
+docker build front-end -t aas-frontend:1.0.1
 # docker push localhost:5001/aas-front-end:latest
