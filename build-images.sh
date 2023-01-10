9b669404523a004fabdf2cee4e4665bbe39d5081
Tien-Cheng <tiencheng@protonmail.com>
2023-01-10 08:57:53 +0800

    [fix] Allow passing custom TLS cert to allow ClearML SDK to connect to self-hosted ClearML instances

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Build image
-docker build back-end -t aas-backend:1.0.1 --build-arg ENV_STATE=prod
+docker build back-end -t aas-backend:1.0.3 --build-arg ENV_STATE=prod
 # docker push localhost:5001/aas-back-end:latest
 docker build front-end -t aas-frontend:1.0.3 
 # docker push localhost:5001/aas-front-end:latest
