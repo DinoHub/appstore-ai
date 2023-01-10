c3f9f071d2e66a198daa2ad57df8f4d8c8d693cb
Tien-Cheng <tiencheng@protonmail.com>
2023-01-10 11:34:29 +0800

    [feat] Make dark mode setting persist

--- front-end/src/components/layout/DarkModeToggle.vue
+++ front-end/src/components/layout/DarkModeToggle.vue
@@ -9,14 +9,27 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { useQuasar } from 'quasar';
 const $q = useQuasar();
 const toggleDarkMode = () => {
   $q.dark.toggle();
+  localStorage.setItem('theme', $q.dark.isActive ? 'dark' : 'light');
   if (document.documentElement.getAttribute('data-theme') === 'dark') {
     document.documentElement.setAttribute('data-theme', 'light');
   } else {
     document.documentElement.setAttribute('data-theme', 'dark');
   }
 };
+
+onMounted(() => {
+  // Check localStorage for theme
+  const theme = localStorage.getItem('theme');
+  $q.dark.set(theme === 'dark');
+  if (theme === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+});
 </script>
