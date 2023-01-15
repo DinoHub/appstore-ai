da7944cdcfdbadbdac8d8ec263077b7b90625292
Tien-Cheng <tiencheng@protonmail.com>
2023-01-15 16:28:34 +0800

    [dbg] Adjust kind development script

--- k8s/environments/development/cluster/generate-ip-address-pool-kind.sh
+++ k8s/environments/development/cluster/generate-ip-address-pool-kind.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 network=$(docker network inspect kind -f "{{(index .IPAM.Config 0).Subnet}}" | cut -d '.' -f1,2)
 # Based on this, generate a metallb address pool
+rm -f $(dirname "$0")/metallb-ip-address-pool.yaml
 cat << EOF >> $(dirname "$0")/metallb-ip-address-pool.yaml
 apiVersion: metallb.io/v1beta1
 kind: IPAddressPool
