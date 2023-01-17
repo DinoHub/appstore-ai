3babef6d029f44eae38bf7c8dcf7c4c30abccd6e
Tien-Cheng <tiencheng@protonmail.com>
2023-01-17 11:23:03 +0800

    [ver] Bump image version

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Build image
-APP_VERSION=1.0.6
+APP_VERSION=1.0.7
 docker build back-end -t aas-backend:{APP_VERSION} --build-arg ENV_STATE=prod
 docker build front-end -t aas-frontend:{APP_VERSION}
