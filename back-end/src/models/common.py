72a770888243736aebdf294a22f8158ecd4bffc4
Mathias Ho <osirishyp@gmail.com>
2023-01-24 21:55:57 +0800

    [update] new model for dict to store bucket and object name

--- back-end/src/models/common.py
+++ back-end/src/models/common.py
@@ -51,3 +51,9 @@ class PyObjectId(ObjectId):
     @classmethod
     def __modify_schema__(cls, field_schema):
         field_schema.update(type="string")
+
+
+class S3Storage(BaseModel):
+
+    bucket_name: str
+    object_name: str
