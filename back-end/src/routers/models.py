d158754fe5f37d22521fe882c1643b09a29aca39
Mathias Ho <osirishyp@gmail.com>
2023-02-07 16:59:15 +0800

    [update] export models route will now call the bg task for exporting models so it will run in the bg instead

--- back-end/src/routers/models.py
+++ back-end/src/routers/models.py
@@ -38,7 +38,11 @@ from ..internal.preprocess_html import (
     preprocess_html_post,
     process_html_to_base64,
 )
-from ..internal.tasks import delete_orphan_images, delete_orphan_services
+from ..internal.tasks import (
+    delete_orphan_images,
+    delete_orphan_services,
+    export_selected_models,
+)
 from ..internal.utils import uncased_to_snake_case
 from ..models.common import S3Storage
 from ..models.iam import TokenData
@@ -48,7 +52,7 @@ from ..models.model import (
     ModelCardModelIn,
     SearchModelResponse,
     UpdateModelCardModel,
-    modelCardPackage,
+    ModelCardPackage,
 )
 
 CHUNK_SIZE = 1024
@@ -581,7 +585,7 @@ async def delete_model_card_by_id(
 
 @router.delete("/multi", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_multiple_model_cards(
-    card_package: modelCardPackage,
+    card_package: ModelCardPackage,
     tasks: BackgroundTasks,
     db=Depends(get_db),
     user: TokenData = Depends(get_current_user),
@@ -589,7 +593,7 @@ async def delete_multiple_model_cards(
     """Delete multiple model cards by List of composite keys of Creator User ID and Model ID
 
     Args:
-        card_package (modelCardPackage): List of Dictionaries containing models with composite ID identifiers to be removed
+        card_package (ModelCardPackage): List of Dictionaries containing models with composite ID identifiers to be removed
         card (UpdateModelCardModel): Updated model card
         tasks (BackgroundTasks): Background tasks to run
         db (Tuple[AsyncIOMotorDatabase, AsyncIOMotorClient], optional): MongoDB connection.
@@ -637,12 +641,12 @@ async def delete_multiple_model_cards(
 
 @router.post(
     "/export",
-    status_code=status.HTTP_204_NO_CONTENT,
+    status_code=status.HTTP_200_OK,
     dependencies=[Depends(check_is_admin)],
 )
 async def export_models(
-    card_package: modelCardPackage,
-    db=Depends(get_db),
+    card_package: ModelCardPackage,
+    tasks: BackgroundTasks,
     user: TokenData = Depends(get_current_user),
     s3_client: Minio = Depends(minio_api_client),
 ):
@@ -654,282 +658,7 @@ async def export_models(
                 detail="User does not have sufficient privilege to export models!",
             )
         else:
-            db, mongo_client = db
-            pkg = card_package.dict()["card_package"]
-            current_time_stringified = datetime.datetime.now().strftime(
-                "%Y-%m-%d_%H:%M:%S.%f"
-            )
-            current_time = str(datetime.datetime.now())
-            BUCKET_NAME = config.MINIO_BUCKET_NAME or "default"
-
-            async with await mongo_client.start_session() as session:
-                async with session.start_transaction():
-                    await db["exports"].insert_one(
-                        {
-                            "userId": user.user_id,
-                            "timeInitiated": current_time,
-                            "models": pkg,
-                        }
-                    )
-                    for x in pkg:
-                        try:
-                            existing_card = await db["models"].find_one(
-                                {
-                                    "modelId": x["model_id"],
-                                    "creatorUserId": x["creator_user_id"],
-                                }
-                            )
-                            existing_card["markdown"] = process_html_to_base64(
-                                existing_card["markdown"]
-                            )
-                            existing_card["performance"] = process_html_to_base64(
-                                existing_card["performance"]
-                            )
-                            subfile_name = f'{x["creator_user_id"]}-{x["model_id"]}'
-                            dumped_JSON: str = json.dumps(
-                                existing_card,
-                                ensure_ascii=False,
-                                indent=4,
-                                sort_keys=True,
-                                default=str,
-                            )
-                            try:
-                                upload_data(
-                                    s3_client,
-                                    dumped_JSON.encode("utf-8"),
-                                    f"exports/{current_time_stringified}/{subfile_name}/card-metadata.json",
-                                    BUCKET_NAME,
-                                    "application/json",
-                                )
-                            except Exception as err:
-                                await db["exports"].update_one(
-                                    {
-                                        "userId": user.user_id,
-                                        "timeInitiated": current_time,
-                                        "models.model_id": x["model_id"],
-                                        "models.creator_user_id": x["creator_user_id"],
-                                    },
-                                    {
-                                        "$set": {
-                                            "models.$.progress": "Failed",
-                                        },
-                                        "$push": {
-                                            "models.$.reason": "Card metadata could not be retrieved",
-                                        },
-                                    },
-                                )
-                                print(
-                                    f"{Fore.YELLOW}WARNING{Fore.WHITE}:  Could not retrieve card metadata info. Skipping...!"
-                                )
-                            try:
-
-                                artifactSet = existing_card["artifacts"]
-                                modelFileLocation = str(
-                                    list(
-                                        filter(
-                                            lambda d: d["artifactType"] == "mainModel",
-                                            artifactSet,
-                                        )
-                                    )[0]["url"]
-                                )
-                                (model_bucket, model_object) = modelFileLocation.split(
-                                    "s3://"
-                                )[1].split("/", 1)
-                                compose_data(
-                                    s3_client,
-                                    [
-                                        S3Storage(
-                                            bucket_name=model_bucket,
-                                            object_name=model_object,
-                                        )
-                                    ],
-                                    f"exports/{current_time_stringified}/{subfile_name}/{model_object.split('/')[-1]}",
-                                    BUCKET_NAME,
-                                )
-                            except Exception as err:
-                                await db["exports"].update_one(
-                                    {
-                                        "userId": user.user_id,
-                                        "timeInitiated": current_time,
-                                        "models.model_id": x["model_id"],
-                                        "models.creator_user_id": x["creator_user_id"],
-                                    },
-                                    {
-                                        "$set": {
-                                            "models.$.progress": "Failed",
-                                        },
-                                        "$push": {
-                                            "models.$.reason": "Model file could not be retrieved",
-                                        },
-                                    },
-                                )
-                                print(
-                                    f"{Fore.YELLOW}WARNING{Fore.WHITE}:  Could not retrieve model file. Skipping...!"
-                                )
-                            if (
-                                existing_card["task"] == "Reinforcement Learning"
-                                and existing_card["videoLocation"] is not None
-                            ):
-                                try:
-                                    url: str = existing_card["videoLocation"]
-                                    url = url.removeprefix("s3://")
-                                    bucket, object_name = url.split("/", 1)
-                                    response = get_data(s3_client, object_name, bucket)
-
-                                    file_extension = object_name.split(".").pop()
-                                    upload_data(
-                                        s3_client,
-                                        response.data,
-                                        f"exports/{current_time_stringified}/{subfile_name}/example-video.{file_extension}",
-                                        BUCKET_NAME,
-                                        f"video/{file_extension}",
-                                    )
-                                    response.close()
-                                    response.release_conn()
-                                except Exception as err:
-                                    await db["exports"].update_one(
-                                        {
-                                            "userId": user.user_id,
-                                            "timeInitiated": current_time,
-                                            "models.model_id": x["model_id"],
-                                            "models.creator_user_id": x[
-                                                "creator_user_id"
-                                            ],
-                                        },
-                                        {
-                                            "$set": {
-                                                "models.$.progress": "Failed",
-                                            },
-                                            "$push": {
-                                                "models.$.reason": "Example video could not be retrieved",
-                                            },
-                                        },
-                                    )
-                                    print(
-                                        f"{Fore.YELLOW}WARNING{Fore.WHITE}:  Could not retrieve video from bucket. Skipping...!"
-                                    )
-                            else:
-                                try:
-                                    existing_service = await db["services"].find_one(
-                                        {
-                                            "modelId": x["model_id"],
-                                            "creatorUserId": x["creator_user_id"],
-                                        }
-                                    )
-                                    dumped_JSON_service: str = json.dumps(
-                                        existing_service,
-                                        ensure_ascii=False,
-                                        indent=4,
-                                        sort_keys=True,
-                                        default=str,
-                                    )
-                                    upload_data(
-                                        s3_client,
-                                        dumped_JSON_service.encode("utf-8"),
-                                        f"exports/{current_time_stringified}/{subfile_name}/service-metadata.json",
-                                        BUCKET_NAME,
-                                        "application/json",
-                                    )
-                                except Exception as err:
-                                    await db["exports"].update_one(
-                                        {
-                                            "userId": user.user_id,
-                                            "timeInitiated": current_time,
-                                            "models.model_id": x["model_id"],
-                                            "models.creator_user_id": x[
-                                                "creator_user_id"
-                                            ],
-                                        },
-                                        {
-                                            "$set": {
-                                                "models.$.progress": "Failed",
-                                            },
-                                            "$push": {
-                                                "models.$.reason": "Service metadata could not be retrieved",
-                                            },
-                                        },
-                                    )
-                                    print(
-                                        f"{Fore.YELLOW}WARNING{Fore.WHITE}:  Could not retrieve service info from database. Skipping...!"
-                                    )
-                            log = await db["exports"].find_one(
-                                {
-                                    "userId": user.user_id,
-                                    "timeInitiated": current_time,
-                                },
-                                {
-                                    "models": {
-                                        "$elemMatch": {
-                                            "model_id": x["model_id"],
-                                            "creator_user_id": x["creator_user_id"],
-                                        }
-                                    },
-                                },
-                            )
-                            if not "progress" in log["models"][0].keys():
-                                await db["exports"].update_one(
-                                    {
-                                        "userId": user.user_id,
-                                        "timeInitiated": current_time,
-                                        "models.model_id": x["model_id"],
-                                        "models.creator_user_id": x["creator_user_id"],
-                                    },
-                                    {
-                                        "$set": {
-                                            "models.$.progress": "Completed",
-                                        }
-                                    },
-                                )
-                            elif log["models"][0]["progress"] != "Failed":
-                                await db["exports"].update_one(
-                                    {
-                                        "userId": user.user_id,
-                                        "timeInitiated": current_time,
-                                        "models.model_id": x["model_id"],
-                                        "models.creator_user_id": x["creator_user_id"],
-                                    },
-                                    {
-                                        "$set": {
-                                            "models.$.progress": "Completed",
-                                        }
-                                    },
-                                )
-                        except Exception as err:
-                            await db["exports"].update_one(
-                                {
-                                    "userId": user.user_id,
-                                    "timeInitiated": current_time,
-                                    "models.model_id": x["model_id"],
-                                    "models.creator_user_id": x["creator_user_id"],
-                                },
-                                {
-                                    "$set": {
-                                        "models.$.progress": "Failed",
-                                    },
-                                    "$push": {
-                                        "models.$.reason": "Unexpected error",
-                                    },
-                                },
-                            )
-                            print(
-                                f"{Fore.YELLOW}WARNING{Fore.WHITE}:  Unexpected error was returned: {err}. Skipping...!"
-                            )
-                            continue
-                    await db["exports"].update_one(
-                        {
-                            "userId": user.user_id,
-                            "timeInitiated": current_time,
-                        },
-                        {
-                            "$set": {
-                                "timeCompleted": str(datetime.datetime.now()),
-                                "exportLocation": f"s3://{BUCKET_NAME}/exports/{current_time_stringified}",
-                            }
-                        },
-                    )
-            return
-    except HTTPException as err:
-        raise err
+            tasks.add_task(export_selected_models, card_package, user, s3_client)
     except Exception as err:
         print(err)
         raise HTTPException(
