8917d502eb3dcbbb401052090092e93b65380b00
Mathias Ho <osirishyp@gmail.com>
2023-02-07 16:48:00 +0800

    [new] model for getting export logs in paginated format

--- back-end/src/models/exports.py
+++ back-end/src/models/exports.py
@@ -1,6 +1,6 @@
 """Data models for exports"""
 from datetime import datetime
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Dict
 
 from bson import ObjectId
 from pydantic import BaseModel, Field, validator
@@ -13,4 +13,58 @@ class ExportsPage(BaseModel):
     exports_num: int = 5
     userId: str = ""
     time_initiated_range: Union[str, dict, None] = {"from": "", "to": ""}
-    time_created_range: Union[str, dict, None] = {"from": "", "to": ""}
+    time_completed_range: Union[str, dict, None] = {"from": "", "to": ""}
+
+    @validator("userId")
+    def id_is_empty(cls, v: str) -> Optional[str]:
+        """Validate user id search field is not empty
+
+        Args:
+            v (str): User id
+
+        Returns:
+            Optional[str]: user id if not empty, None if empty
+        """
+        if v.strip() == "":
+            return None
+        return v
+
+    @validator("time_initiated_range")
+    def time_initiated_check(cls, v: Dict) -> Optional[Dict]:
+        """Check if time initiated search range is empty
+
+        Args:
+            v (Dict): Time initiated search range
+
+        Returns:
+            Optional[Dict]: Search range if not empty, None if empty
+        """
+        if v["from"] == "" or v["to"] == "" or v == "":
+            return None
+        return v
+
+    @validator("time_completed_range")
+    def time_completed_check(cls, v: Dict) -> Optional[Dict]:
+        """Check if time completed search range is empty
+
+        Args:
+            v (Dict): Time completed search range
+
+        Returns:
+            Optional[Dict]: Search range if not empty, None if empty
+        """
+        if v["from"] == "" or v["to"] == "" or v == "":
+            return None
+        return v
+
+
+class ExportLog(BaseModel):
+
+    userId: str = ""
+    time_initiated: str = ""
+    time_completed: str = ""
+
+
+class ExportLogPackage(BaseModel):
+
+    logs_package: List[ExportLog]
