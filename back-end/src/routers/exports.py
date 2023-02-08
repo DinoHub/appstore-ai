8049520c933bcb162bd39f1763510bee8d328d6e
Mathias Ho <osirishyp@gmail.com>
2023-02-08 17:48:52 +0800

    [update] finish route to remove export logs in db and related export in s3 bucket

--- back-end/src/routers/exports.py
+++ back-end/src/routers/exports.py
@@ -1,4 +1,5 @@
 from typing import Dict, List, Optional, Tuple
+from colorama import Fore
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from pymongo import ASCENDING, DESCENDING
 from minio import Minio
@@ -10,12 +11,16 @@ from fastapi import (
     Query,
     status,
 )
+from fastapi.responses import JSONResponse
 
 from ..models.exports import ExportsPage, ExportLogPackage
 
 from ..internal.auth import check_is_admin
 from ..internal.dependencies.mongo_client import get_db
-from ..internal.dependencies.minio_client import minio_api_client
+from ..internal.dependencies.minio_client import (
+    minio_api_client,
+    remove_data_from_prefix,
+)
 
 router = APIRouter(prefix="/exports", tags=["Exports"])
 
@@ -91,10 +96,43 @@ async def get_exported(
 @router.delete(
     "/", status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(check_is_admin)]
 )
-def remove_exports(
+async def remove_exports(
     log_package: ExportLogPackage,
     s3_client: Minio = Depends(minio_api_client),
     db: Tuple[AsyncIOMotorDatabase, AsyncIOMotorClient] = Depends(get_db),
 ):
     db, mongo_client = db
-    log_package.logs_package
+    exports_list = log_package.logs_package
+    failed_exports_removals = []
+    async with await mongo_client.start_session() as session:
+        async with session.start_transaction():
+            check = await (
+                db["exports"]
+                .find({"$or": list(map(lambda x: x.dict(by_alias=True), exports_list))})
+                .to_list(len(exports_list))
+            )
+
+            for x in check:
+                (model_bucket, prefix) = (
+                    x["exportLocation"].split("s3://")[1].split("/", 1)
+                )
+                errors = remove_data_from_prefix(s3_client, prefix, model_bucket)
+                for error in errors:
+                    if error is not None:
+                        failed_exports_removals.append(x["exportLocation"])
+                        print(
+                            f"{Fore.YELLOW}WARNING{Fore.WHITE}:  Removal of export '{prefix}' from S3 bucket failed! Skipping..."
+                        )
+                        continue
+                await db["exports"].delete_one(
+                    {
+                        "userId": x["userId"],
+                        "timeInitiated": x["timeInitiated"],
+                        "timeCompleted": x["timeCompleted"],
+                    }
+                )
+            if len(failed_exports_removals) > 0:
+                return JSONResponse(
+                    status_code=status.HTTP_206_PARTIAL_CONTENT,
+                    content=f"Exports of {str(failed_exports_removals)} failed",
+                )
