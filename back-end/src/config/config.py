113ac0661e4ca2ff73744934ed11a8a7b13976dc
Mathias Ho <osirishyp@gmail.com>
2023-04-03 13:08:27 +0800

    [fix] remove ADMIN_SECRET_KEY as it's useless

--- back-end/src/config/config.py
+++ back-end/src/config/config.py
@@ -40,7 +40,6 @@ class GlobalConfig(BaseSettings):
     SECRET_KEY: Optional[
         str
     ] = None  # NOTE: set to none as a hack to get Sphinx to build correctly
-    ADMIN_SECRET_KEY: Optional[str] = None
     FIRST_SUPERUSER_ID: Optional[str] = None
     FIRST_SUPERUSER_PASSWORD: Optional[str] = None
 
