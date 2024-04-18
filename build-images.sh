a65cc98ea05139c6a5959caa2b77320801564df6
jovanhuang <jhtc98@hotmail.com>
2024-04-18 14:56:05 +0800

    Enable Keycloak configuration to be editable via Frontend Helm Chart overrides.

--- build-images.sh
+++ build-images.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # Build image
-APP_VERSION=${1:-'1.2.0'}
-DOCKER_USERNAME=<docker_username>
-docker build back-end -t docker.io/$DOCKER_USERNAME/aas-backend:$APP_VERSION --build-arg ENV_STATE=prod
-docker build front-end -t docker.io/$DOCKER_USERNAME/aas-frontend:$APP_VERSION
+FRONTEND_APP_VERSION=${1:-'1.2.1'}
+BACKEND_APP_VERSION=${1:-'1.2.0'}
 
-docker push docker.io/$DOCKER_USERNAME/aas-backend:$APP_VERSION
-docker push docker.io/$DOCKER_USERNAME/aas-frontend:$APP_VERSION
+DOCKER_USERNAME=<docker_username>
+
+docker build back-end -t docker.io/$DOCKER_USERNAME/aas-backend:$BACKEND_APP_VERSION --build-arg ENV_STATE=prod
+docker build front-end -t docker.io/$DOCKER_USERNAME/aas-frontend:$FRONTEND_APP_VERSION
+
+docker push docker.io/$DOCKER_USERNAME/aas-backend:$BACKEND_APP_VERSION
+docker push docker.io/$DOCKER_USERNAME/aas-frontend:$FRONTEND_APP_VERSION
 
 echo "y" | docker system prune
 
--- front-end/entrypoint.sh
+++ front-end/entrypoint.sh
@@ -4,8 +4,16 @@ ROOT_DIR=$(pwd)
 # Replace environment variables in compiled JS files
 # This is done to avoid having to rebuild the image every time the backend URL changes
 echo "Configuring backend URL to ${VUE_APP_BACKEND_URL}"
+echo "Configuring VUE_APP_KEYCLOAK_CLIENT_ID_ENV to ${VUE_APP_KEYCLOAK_CLIENT_ID_ENV}"
+echo "Configuring VUE_APP_KEYCLOAK_URL_ENV to ${VUE_APP_KEYCLOAK_URL_ENV}"
+echo "Configuring VUE_APP_KEYCLOAK_REALM_ENV to ${VUE_APP_KEYCLOAK_REALM_ENV}"
+
+# Switch to the node user to execute sed with appropriate permissions
 for file in $(find $ROOT_DIR/dist/spa/assets -name '*.js'); do
     echo "Processing $file"
+    sed -i "s|VUE_APP_KEYCLOAK_CLIENT_ID_ENV|${VUE_APP_KEYCLOAK_CLIENT_ID_ENV}|g" $file
+    sed -i "s|VUE_APP_KEYCLOAK_URL_ENV|${VUE_APP_KEYCLOAK_URL_ENV}|g" $file
+    sed -i "s|VUE_APP_KEYCLOAK_REALM_ENV|${VUE_APP_KEYCLOAK_REALM_ENV}|g" $file
     sed -i "s|VUE_APP_BACKEND_URL|${VUE_APP_BACKEND_URL}|g" $file
 done
 
